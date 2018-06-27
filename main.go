53f7477fe6953cb27453adab0fa3829918295445
Matthew Hodgkins <matthodge@gmail.com>
2018-06-27 23:25:13 +0200

    Add main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform/plugin"
+
+	"github.com/MattHodge/terraform-provider-octopusdeploy/octopusdeploy"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: octopusdeploy.Provider})
+}
