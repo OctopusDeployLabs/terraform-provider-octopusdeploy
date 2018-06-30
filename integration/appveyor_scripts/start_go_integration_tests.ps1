8469e1ae03cf6b077dac5566d50270029d1b2b4b
Matthew Hodgkins <matthodge@gmail.com>
2018-06-30 16:12:31 +0200

    Enable TF_ACC for Acceptance Tests

--- integration/appveyor_scripts/start_go_integration_tests.ps1
+++ integration/appveyor_scripts/start_go_integration_tests.ps1
@@ -7,4 +7,4 @@ foreach ($function in $functionFolder) { . $function.FullName }
 $OCTOPUS_URL = "http://localhost"
 $OCTOPUS_APIKEY = Get-OctopusDeployApiKey -OctopusUrl $OCTOPUS_URL -Username $env:TEST_OCTOPUS_USERNAME -Password $env:TEST_OCTOPUS_PASSWORD
 
-Start-ProcessAdvanced -FilePath 'go' -ArgumentList "test -v -timeout 30s ./octopusdeploy/..." -EnvironmentKeyValues @{ OCTOPUS_URL = $OCTOPUS_URL; OCTOPUS_APIKEY = $OCTOPUS_APIKEY } -Verbose
+Start-ProcessAdvanced -FilePath 'go' -ArgumentList "test -v -timeout 30s ./octopusdeploy/..." -EnvironmentKeyValues @{ OCTOPUS_URL = $OCTOPUS_URL; OCTOPUS_APIKEY = $OCTOPUS_APIKEY; TF_ACC = 1 } -Verbose
