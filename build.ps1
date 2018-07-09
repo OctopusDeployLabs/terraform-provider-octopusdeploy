e67bc25c1557f2127ffa2e27053c10e03069f815
Matthew Hodgkins <matthodge@gmail.com>
2018-07-09 21:48:35 +0200

    Improve Golang Build

--- build.ps1
+++ build.ps1
@@ -1,3 +1,12 @@
+Param
+(
+    # Param1 help description
+    [Parameter(Mandatory=$true)]
+    [string]
+    $BuildVersion
+)
+
+
 . ".\integration\appveyor_scripts\functions\Start-ProcessAdvanced.ps1"
 
 $binaries = @(
@@ -29,13 +38,22 @@ $binaries = @(
 
 
 foreach ($binary in $binaries){
-	$buildOutputPath = "$($binary.goos)-$($binary.goarch)"
+    $buildName = "$($binary.goos)-$($binary.goarch)"
+	$buildOutputPath = "build\$($buildName)"
 
 	if ($binary.goos -eq "windows") {
 		$fileExtension = ".exe"
 	}
 
-	New-Item -Path $buildOutputPath -Force
+	New-Item -Path $buildOutputPath -ItemType Directory -Force
 
-	Start-ProcessAdvanced -FilePath 'go' -ArgumentList "build -o $($buildOutputPath)/terraform-provider-octopusdeploy$($fileExtension)" -EnvironmentKeyValues @{ GOOS = $binary.goos; GOARCH = $binary.goarch; } -Verbose
+	Start-ProcessAdvanced -FilePath 'go' -ArgumentList "build -o $($buildOutputPath)/terraform-provider-octopusdeploy$($fileExtension)" -EnvironmentKeyValues @{ GOOS = $binary.goos; GOARCH = $binary.goarch; GOPATH = 'C:\gopath' } -Verbose
+
+    Push-Location $buildOutputPath
+
+    Compress-Archive -Path . -DestinationPath "terraform-provider-octopusdeploy-$($buildName)-$($BuildVersion).zip"
+
+    Move-Item "terraform-provider-octopusdeploy-$($buildName)-$($BuildVersion).zip" ..
+
+    Pop-Location
 }
