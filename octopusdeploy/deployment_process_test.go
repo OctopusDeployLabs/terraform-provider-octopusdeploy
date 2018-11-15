973a77d3f74aa3f1b3ce4e45d1b6a1267a91052c
Robert Wagner <robert@wagner.id.au>
2018-11-15 16:35:48 +1000

    Most of the generic deployment process

---
+++ octopusdeploy/deployment_process_test.go
@@ -0,0 +1,144 @@
+package octopusdeploy
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/MattHodge/go-octopusdeploy/octopusdeploy"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccOctopusDeployDeploymentProcessBasic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckOctopusDeployDeploymentProcessDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDeploymentProcessBasic(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOctopusDeployDeploymentProcess(),
+				),
+			},
+		},
+	})
+}
+
+func testAccDeploymentProcessBasic() string {
+	return `
+		resource "octopusdeploy_lifecycle" "test" {
+			name = "Test Lifecycle"
+		}
+
+		resource "octopusdeploy_project_group" "test" {
+			name = "Test Group"
+		}
+
+		resource "octopusdeploy_project" "test" {
+			name             = "Test Project"
+			lifecycle_id     = "${octopusdeploy_lifecycle.test.id}"
+			project_group_id = "${octopusdeploy_project_group.test.id}"
+		}
+
+		resource "octopusdeploy_deployment_process" "test" {
+			project_id = "${octopusdeploy_project.test.id}"
+
+			step {
+				name = "Test"
+				target_roles = ["A", "B"]
+				package_requirement = "BeforePackageAcquisition"
+				condition = "Variable"
+				condition_expression = "#{run}"
+				start_trigger = "StartWithPrevious"
+				window_size = "5"
+				
+
+				action {
+					name = "Test"
+					action_type = "Octopus.Script"
+					disabled = false
+					required = true
+					worker_pool_id = "WorkerPools-1"
+					environments = ["Environments-1"]
+					excluded_environments = ["Environments-2"]
+					channels = ["Channels-1"]
+					tenant_tags = ["tag/tag"]
+					
+					primary_package {
+						package_id = "MyPackage"
+						feed_id = "feeds-builtin"
+						acquisition_location = "ExecutionTarget"
+					}
+
+					package {
+						name = "ThePackage"
+						package_id = "MyPackage"
+						feed_id = "feeds-builtin"
+						acquisition_location = "NotAcquired"
+						extract_during_deployment = true
+
+						property {
+							key = "WhatIsThis"
+							value = "Dunno"
+						}
+
+					}
+
+					property {
+						key = "Octopus.Action.RunOnServer"
+						value = "true"
+					}
+				}
+			}
+		}
+		`
+}
+
+func testAccCheckOctopusDeployDeploymentProcessDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*octopusdeploy.Client)
+
+	if err := destroyProjectHelper(s, client); err != nil {
+		return err
+	}
+	if err := destroyHelperProjectGroup(s, client); err != nil {
+		return err
+	}
+	if err := destroyHelperLifecycle(s, client); err != nil {
+		return err
+	}
+	return nil
+}
+
+func testAccCheckOctopusDeployDeploymentProcess() resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*octopusdeploy.Client)
+
+		process, err := getDeploymentProcess(s, client);
+		if err != nil {
+			return err
+		}
+
+		expectedNumberOfSteps := 1
+		numberOfSteps := len(process.Steps)
+		if numberOfSteps != expectedNumberOfSteps {
+			return fmt.Errorf("Deployment process has %d steps instead of the expected %d", numberOfSteps, expectedNumberOfSteps)
+		}
+
+		if process.Steps[0].Actions[0].Properties["Octopus.Action.RunOnServer"] != "true" {
+			return fmt.Errorf("The RunOnServer property has not been set to true on the deployment process")
+		}
+
+		return nil;
+	}
+}
+
+
+func getDeploymentProcess(s *terraform.State, client *octopusdeploy.Client) (*octopusdeploy.DeploymentProcess, error) {
+	for _, r := range s.RootModule().Resources {
+		if r.Type == "octopusdeploy_deployment_process" {
+			return client.DeploymentProcess.Get(r.Primary.ID);
+		}
+	}
+	return nil, fmt.Errorf("No deployment process found in the terraform resources")
+}
