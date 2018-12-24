355d51b04d40a5c886cab6699fb1ca15182a8997
Matthew Hodgkins <matthodge@gmail.com>
2018-12-24 16:12:36 +0100

    Allow Pull Request to work Locally

--- ci-scripts/pull_request.sh
+++ ci-scripts/pull_request.sh
@@ -5,9 +5,19 @@ set -eo pipefail
 go mod download
 
 # Lint
-curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s v1.12.4
 
-./bin/golangci-lint run
+## When in CI, install linter and run from a special path
+if [ -n "${CI}" ]; then
+    curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s v1.12.4
+
+    ./bin/golangci-lint run
+else
+    golangci-lint run
+fi
 
 # Test
+
+## Enable TF Acceptance Tests
+export TF_ACC=1
+
 go test -v -timeout 240s ./octopusdeploy/...
