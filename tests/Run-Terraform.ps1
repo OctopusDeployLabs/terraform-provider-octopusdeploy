7a07f4f00a73111f94bebbb8edb90165be605cf2
Matthew Casperson <matthewcasperson@gmail.com>
2020-05-26 12:49:01 +1000

    Added more logging

--- tests/Run-Terraform.ps1
+++ tests/Run-Terraform.ps1
@@ -7,7 +7,7 @@ Write-Host "finding plugin for $operatingSystem"
 Write-Host "The contents of the build directory:"
 Write-Host (ls build)
 
-$dir = ls build | ? {$_.Name -like "*$operatingSystem*"}
+$dir = ls build | ? {$_.Name -like "*$operatingSystem*"} | Select -First 1
 Write-Host "Plugin dir is set to $($dir.Name)"
 
 terraform init -plugin-dir "./build/$($dir.Name)" tests
