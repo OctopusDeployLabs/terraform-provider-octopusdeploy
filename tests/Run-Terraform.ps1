115ba13149f464273a1140a1c98cafb419cd7d6b
Matthew Casperson <matthewcasperson@gmail.com>
2020-05-26 12:39:37 +1000

    Fixing calling terraform

--- tests/Run-Terraform.ps1
+++ tests/Run-Terraform.ps1
@@ -2,7 +2,11 @@ param (
     [string]$operatingSystem = "linux_amd64"
 )
 
+Write-Host (ls build)
+
 $dir = ls build | ? {$_.Name -like "*$operatingSystem*"}
+
+Write-Host "plugin dir is set to $($dir.Name)"
 
 terraform init -plugin-dir "./build/$($dir.Name)" tests
 terraform apply -auto-approve tests
--- tests/Terraform.Tests.ps1
+++ tests/Terraform.Tests.ps1
@@ -1,10 +1,11 @@
 . $PSScriptRoot\Octopus.ps1
 
 Describe 'Terraform Provider' {
-    It 'must create a project group called Test' {
-        Wait-ForOctopus
+    It 'project-group.tf must create a project group called Test' {
         $repository = Connect-ToOctopus http://localhost:8080
         $entity = Invoke-ScriptBlockWithRetries {$repository.ProjectGroups.FindByName("Test")}
         $entity | Should -Not -Be $null
+        $entity.Name | Should -Be "Test"
+        $entity.Description | Should -Be "Test Applications"
     }
 }
