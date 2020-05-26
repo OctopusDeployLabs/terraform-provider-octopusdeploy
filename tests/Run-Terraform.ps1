ff36a173ae6e45643f69dbbc170bd455df6a6f3f
Matthew Casperson <matthewcasperson@gmail.com>
2020-05-26 11:02:30 +1000

    Switched to linux

--- tests/Run-Terraform.ps1
+++ tests/Run-Terraform.ps1
@@ -1,4 +1,4 @@
-$dir = ls build | ? {$_.Name -like "*windows_amd64*"}
+$dir = ls build | ? {$_.Name -like "*linux_amd64*"}
 
 terraform init -plugin-dir "./build/$($dir.Name)" tests
 terraform apply -auto-approve tests
