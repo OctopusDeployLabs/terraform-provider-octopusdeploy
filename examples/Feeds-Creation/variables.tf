cf3cad3b61502406aa07e0b7cc5b1ca40eae6367
Michael Levan <mlevan1992@gmail.com>
2020-08-03 14:38:58 -0400

    Feed example creation

---
+++ examples/Feeds-Creation/variables.tf
@@ -0,0 +1,19 @@
+variable "apiKey" {
+  type = string
+}
+
+variable "space" {
+  type = string
+}
+
+variable "serverURL" {
+  type = string
+}
+
+variable "feedName" {
+  type = string
+}
+
+variable "feed_uri" {
+  type = string
+}
