42e85abff373d4a27e03e9091dff4ce67348d049
Michael Levan <mlevan1992@gmail.com>
2020-08-03 14:06:50 -0400

    Added read, update, and delete functionality to user resource

--- octopusdeploy/resource_user.go
+++ octopusdeploy/resource_user.go
@@ -58,3 +58,41 @@ func resourceUserCreate(d *schema.ResourceData, m interface{}) error {
 
 	return nil
 }
+
+func resourceUserRead(d *schema.ResourceData, m interface{}) error {
+	client := m.(*octopusdeploy.Client)
+
+	userID := d.Id()
+
+	user, err := client.User.Get(userID)
+
+	if err == octopusdeploy.ErrItemNotFound {
+		d.SetId("")
+		return nil
+	}
+
+	if err != nil {
+		return fmt.Errorf("error reading user id %s: %s", user.ID, err.Error())
+	}
+
+	log.Printf("[DEBUG] user: %v", m)
+	d.Set("name", user.UserName)
+	d.Set("description", user.Description)
+	return nil
+}
+
+func resourceUserUpdate(d *schema.ResourceData, m interface{}) error {
+	user := buildProjectGroupResource(d)
+	user.ID = d.Id()
+
+	client := m.(*octopusdeploy.Client)
+
+	updatedUser, err := client.User.Update(user)
+
+	if err != nil {
+		return fmt.Errorf("error updating user id %s: %s", d.Id(), err.Error())
+	}
+
+	d.SetId(updatedUserID)
+	return nil
+}
