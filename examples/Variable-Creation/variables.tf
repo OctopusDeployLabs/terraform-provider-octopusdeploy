91463238313d294887872d24d403e8e034b9c1e0
Michael Levan <mlevan1992@gmail.com>
2020-08-04 15:35:23 -0400

    Create a variable example

---
+++ examples/Variable-Creation/variables.tf
@@ -0,0 +1,19 @@
+variable "apiKey" {
+    type = string
+}
+
+variable "space" {
+    type = string
+}
+
+variable "serverURL" {
+    type = string
+}
+
+variable "projectID" {
+    type = string
+}
+
+variable "varName" {
+    type = string
+}
