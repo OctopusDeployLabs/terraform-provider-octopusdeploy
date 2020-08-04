c16ad0b698019a0fc35666e1366093ef80f96979
Michael Levan <mlevan1992@gmail.com>
2020-08-04 15:19:41 -0400

    Example created for created a deployment target trigger

---
+++ examples/Deployment-Trigger-Creation/variables.tf
@@ -0,0 +1,19 @@
+variable "apiKey" {
+    type = string
+}
+
+variable "space" {
+    type = string
+}
+
+variable "serverURL" {
+    type = string
+}
+
+variable "name" {
+    type = string
+}
+
+variable "projectID" {
+    type = string
+}
