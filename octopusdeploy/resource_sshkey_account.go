cb3365b7e7929ab5afd08a528352eeeacf363d15
AdminTurnedDevOps <mlevan1992@gmail.com>
2020-08-25 18:33:33 -0400

    Added in deployment mode if statements

--- octopusdeploy/resource_sshkey_account.go
+++ octopusdeploy/resource_sshkey_account.go
@@ -54,6 +54,14 @@ func buildSSHKeyResource(d *schema.ResourceData) *octopusdeploy.Account {
 	account.Name = d.Get("username").(string)
 	account.Password = octopusdeploy.SensitiveValue{NewValue: d.Get("password").(string)}
 
+	if v, ok := d.GetOk("tenanted_deployment_participation"); ok {
+		account.TenantedDeploymentParticipation, _ = octopusdeploy.ParseTenantedDeploymentMode(v.(string))
+	}
+
+	if v, ok := d.GetOk("tenant_tags"); ok {
+		account.TenantTags = getSliceFromTerraformTypeList(v)
+	}
+
 	return account
 }
 
