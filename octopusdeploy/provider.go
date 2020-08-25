e445c6d3f7cf0d86aff4bdbabb19e14b70ac681a
Mike Levan <michael.levan@clouddev.engineering>
2020-08-25 17:59:25 -0400

    Removed duplicate

--- octopusdeploy/provider.go
+++ octopusdeploy/provider.go
@@ -39,7 +39,6 @@ func Provider() terraform.ResourceProvider {
 			"octopusdeploy_nuget_feed":                        resourceNugetFeed(),
 			"octopusdeploy_azure_service_principal":           resourceAzureServicePrincipal(),
 			"octopusdeploy_aws_account":           			   resourceAmazonWebServicesAccount(),
-			"octopusdeploy_aws_account":                       resourceAmazonWebServicesAccount(),
 		},
 		Schema: map[string]*schema.Schema{
 			"address": {
