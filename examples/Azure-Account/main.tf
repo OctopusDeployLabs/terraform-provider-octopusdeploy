8fdccd168b75f7ded25fc296c12ba39d9e3153c3
Michael Levan <mlevan1992@gmail.com>
2020-08-28 12:37:24 -0400

    Move resource and create functions

---
+++ examples/Azure-Account/main.tf
@@ -0,0 +1,13 @@
+provider "octopusdeploy" {
+  address = "https://mikelevanoctopus.octopus.app"
+  apikey  = "API-KXTJXUROYOQ3B0WERDRD2NKG48"
+  space   = "Default"
+}
+
+resource "octopusdeploy_azure_service_principal" "Azure" {
+  name = "AzureTesT"
+  client_id = "0d1838a2-0f5c-4988-8cf3-e54c2dcedf42"
+  tenant_id = "2a881dca-3230-4e01-abcb-a1fd235c0981"
+  subscription_number = "f31cfbfd-37fb-4e3d-a713-7bd6ceb2f7bf"
+  key = "3489284023FDSFDSFSD"
+}
