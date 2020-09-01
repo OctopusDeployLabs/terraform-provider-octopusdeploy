582d694fc95551570c398c2017d16b5228121dbc
Robert Wagner <robert@wagner.id.au>
2020-09-01 20:47:17 +1000

    Extracted common account schema and read/update functionality so it can be reused.

---
+++ octopusdeploy/accounts.go
@@ -0,0 +1,139 @@
+package octopusdeploy
+
+import (
+	"fmt"
+	"github.com/OctopusDeploy/go-octopusdeploy/octopusdeploy"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func getCommonAccountsSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"name": {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+		"description": {
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+		"environments": {
+			Type: schema.TypeList,
+			Elem: &schema.Schema{
+				Type: schema.TypeString,
+			},
+			Optional: true,
+		},
+		"tenanted_deployment_participation": getTenantedDeploymentSchema(),
+		"tenants": {
+			Type: schema.TypeList,
+			Elem: &schema.Schema{
+				Type: schema.TypeString,
+			},
+			Optional: true,
+		},
+		"tenant_tags": {
+			Type: schema.TypeList,
+			Elem: &schema.Schema{
+				Type: schema.TypeString,
+			},
+			Optional: true,
+		},
+	}
+}
+
+func fetchAndReadAccount(d *schema.ResourceData, m interface{}) (*octopusdeploy.Account, error) {
+	client := m.(*octopusdeploy.Client)
+
+	accountID := d.Id()
+	account, err := client.Account.Get(accountID)
+
+	if err == octopusdeploy.ErrItemNotFound {
+		d.SetId("")
+		return nil, fmt.Errorf("account %s not found: %s ", accountID, err.Error())
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("error readingaccount %s: %s", accountID, err.Error())
+	}
+
+	d.Set("name", account.Name)
+	d.Set("description", account.Description)
+	d.Set("environments", account.EnvironmentIDs)
+	d.Set("tenanted_deployment_participation", account.TenantedDeploymentParticipation)
+	// TODO TenantID should be named TenantIds and be a string array
+	d.Set("tenants", account.TenantID)
+	d.Set("tenant_tags", account.EnvironmentIDs)
+
+	return account, nil
+}
+
+func buildAccountResourceCommon(d *schema.ResourceData, accountType octopusdeploy.AccountType) *octopusdeploy.Account {
+	var account = octopusdeploy.NewAccount(d.Get("name").(string), accountType);
+
+
+	if v, ok := d.GetOk("tenant_tags"); ok {
+		account.TenantTags = getSliceFromTerraformTypeList(v)
+	}
+
+	if v, ok := d.GetOk("tenanted_deployment_participation"); ok {
+		account.TenantedDeploymentParticipation, _ = octopusdeploy.ParseTenantedDeploymentMode(v.(string))
+	}
+
+	// TODO TenantID should be named TenantIds and be a string array
+	//if v, ok := d.GetOk("tenants"); ok {
+	//	//account.TenantID = getSliceFromTerraformTypeList(v)
+	//}
+
+	if v, ok := d.GetOk("tenant_tags"); ok {
+		account.TenantTags = getSliceFromTerraformTypeList(v)
+	}
+
+
+	return account;
+}
+
+func resourceAccountCreateCommon(d *schema.ResourceData, m interface{}, account *octopusdeploy.Account) error {
+	client := m.(*octopusdeploy.Client)
+
+	account, err := client.Account.Add(account)
+
+	if err != nil {
+		return fmt.Errorf("error creating account %s: %s", account.Name, err.Error())
+	}
+
+	d.SetId(account.ID)
+
+	return nil
+}
+
+
+func resourceAccountUpdateCommon(d *schema.ResourceData, m interface{}, account *octopusdeploy.Account) error {
+	account.ID = d.Id()
+
+	client := m.(*octopusdeploy.Client)
+
+	updatedAccount, err := client.Account.Update(account)
+
+	if err != nil {
+		return fmt.Errorf("error updating username password account id %s: %s", d.Id(), err.Error())
+	}
+
+	d.SetId(updatedAccount.ID)
+	return nil
+}
+
+
+func resourceAccountDeleteCommon(d *schema.ResourceData, m interface{}) error {
+	client := m.(*octopusdeploy.Client)
+
+	accountID := d.Id()
+
+	err := client.Account.Delete(accountID)
+
+	if err != nil {
+		return fmt.Errorf("error deleting username password account id %s: %s", accountID, err.Error())
+	}
+
+	d.SetId("")
+	return nil
+}
--- octopusdeploy/resource_usernamepassword_account.go
+++ octopusdeploy/resource_usernamepassword_account.go
@@ -1,135 +1,68 @@
 package octopusdeploy
 
 import (
-	"fmt"
-
 	"github.com/OctopusDeploy/go-octopusdeploy/octopusdeploy"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
 func resourceUsernamePassword() *schema.Resource {
+
+	schemaMap := getCommonAccountsSchema()
+
+	schemaMap["username"] = &schema.Schema{
+		Type:     schema.TypeString,
+		Optional: true,
+	}
+
+	schemaMap["password"] = &schema.Schema{
+		Type:     schema.TypeString,
+		Optional: true,
+	}
+
 	return &schema.Resource{
 		Create: resourceUsernamePasswordCreate,
 		Read:   resourceUsernamePasswordRead,
 		Update: resourceUsernamePasswordUpdate,
-		Delete: resourceUsernamePasswordDelete,
-
-		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"description": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"tenanted_deployment_participation": getTenantedDeploymentSchema(),
-			"environments": {
-				Type: schema.TypeList,
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
-				},
-				Optional: true,
-			},
-			"account_type": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Default:  "UsernamePassword",
-			},
-			"username": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"password": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-		},
+		Delete: resourceAccountDeleteCommon,
+		Schema: schemaMap,
 	}
 }
 
 func resourceUsernamePasswordRead(d *schema.ResourceData, m interface{}) error {
-	client := m.(*octopusdeploy.Client)
-
-	accountID := d.Id()
-	account, err := client.Account.Get(accountID)
-
-	if err == octopusdeploy.ErrItemNotFound {
-		d.SetId("")
-		return nil
-	}
+	_, err := fetchAndReadAccount(d, m);
+	//account, err := fetchAndReadAccount(d, m);
 
 	if err != nil {
-		return fmt.Errorf("error reading username password account %s: %s", accountID, err.Error())
+		return err;
 	}
 
-	d.Set("name", account.Name)
-	d.Set("description", account.Description)
-	d.Set("environments", account.EnvironmentIDs)
-	d.Set("password", account.Password)
+	// TODO: Username property does not yet exist
+	//d.Set("username", account.Username)
 
-	return nil
+	return nil;
 }
 
 func buildUsernamePasswordResource(d *schema.ResourceData) *octopusdeploy.Account {
-	var account = octopusdeploy.NewAccount(d.Get("name").(string), octopusdeploy.UsernamePassword)
+	account := buildAccountResourceCommon(d, octopusdeploy.UsernamePassword);
 
-	account.Name = d.Get("name").(string)
-	account.Password = octopusdeploy.SensitiveValue{NewValue: d.Get("password").(string)}
+	// TODO: Username property does not yet exist
+	//if v, ok := d.GetOk("username"); ok {
+	//	account.Username = v.(string)
+	//}
 
-	if v, ok := d.GetOk("tenanted_deployment_participation"); ok {
-		account.TenantedDeploymentParticipation, _ = octopusdeploy.ParseTenantedDeploymentMode(v.(string))
-	}
-
-	if v, ok := d.GetOk("tenant_tags"); ok {
-		account.TenantTags = getSliceFromTerraformTypeList(v)
+	if v, ok := d.GetOk("password"); ok {
+		account.Password = octopusdeploy.SensitiveValue{NewValue: v.(string)}
 	}
 
 	return account
 }
 
 func resourceUsernamePasswordCreate(d *schema.ResourceData, m interface{}) error {
-	client := m.(*octopusdeploy.Client)
-
-	newAccount := buildUsernamePasswordResource(d)
-	account, err := client.Account.Add(newAccount)
-
-	if err != nil {
-		return fmt.Errorf("error creating username password account %s: %s", newAccount.Name, err.Error())
-	}
-
-	d.SetId(account.ID)
-
-	return nil
+	account := buildUsernamePasswordResource(d)
+	return resourceAccountCreateCommon(d, m, account);
 }
 
 func resourceUsernamePasswordUpdate(d *schema.ResourceData, m interface{}) error {
 	account := buildUsernamePasswordResource(d)
-	account.ID = d.Id()
-
-	client := m.(*octopusdeploy.Client)
-
-	updatedAccount, err := client.Account.Update(account)
-
-	if err != nil {
-		return fmt.Errorf("error updating username password account id %s: %s", d.Id(), err.Error())
-	}
-
-	d.SetId(updatedAccount.ID)
-	return nil
-}
-
-func resourceUsernamePasswordDelete(d *schema.ResourceData, m interface{}) error {
-	client := m.(*octopusdeploy.Client)
-
-	accountID := d.Id()
-
-	err := client.Account.Delete(accountID)
-
-	if err != nil {
-		return fmt.Errorf("error deleting username password account id %s: %s", accountID, err.Error())
-	}
-
-	d.SetId("")
-	return nil
+	return resourceAccountUpdateCommon(d, m, account)
 }
