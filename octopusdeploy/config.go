bbdcdd3192763e37f00c9044db9f1d6be41f813e
Michael Levan <mlevan1992@gmail.com>
2020-09-16 14:29:03 -0400

    config.go bug fix

--- octopusdeploy/config.go
+++ octopusdeploy/config.go
@@ -19,23 +19,12 @@ func (c *Config) Client() (*client.Client, error) {
 	apiClient, err := client.NewClient(&(http.Client{}), c.Address, c.APIKey, c.Space)
 
 	if c.Space == "" {
+		if err != nil {
+			log.Println(err)
+		}
 
 		log.Printf("[INFO] Octopus Deploy Client configured against default space")
-
-		return apiClient, nil
 	}
 
-	log.Printf("[INFO] Octopus Deploy Client will be scoped to %s space", c.Space)
-
-	space, err := apiClient.Spaces.GetByName(c.Space)
-
-	if err != nil {
-		return nil, err
-	}
-
-	scopedClient, err := client.ForSpace(&(http.Client{}), c.Address, c.APIKey, space)
-
-	log.Printf("[INFO] Octopus Deploy Client configured against %s space", c.Space)
-
-	return scopedClient, nil
+	return apiClient, err
 }
