3faf6914af33e4ccd86e8a4dece53ae941317f93
John Bristowe <john.bristowe@octopus.com>
2020-09-28 06:52:19 +1000

    Added error function

--- octopusdeploy/errors.go
+++ octopusdeploy/errors.go
@@ -8,6 +8,10 @@ func createInvalidParameterError(methodName string, parameterName string) error 
 	return fmt.Errorf("%s: invalid input parameter, %s", methodName, parameterName)
 }
 
+func createResourceOperationError(text string, resourceID string, err error) error {
+	return fmt.Errorf(text, resourceID, err)
+}
+
 func nameIsNil(methodName string) error {
 	return fmt.Errorf("%s: Name is nil", methodName)
 }
