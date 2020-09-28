ab8daa60f8b1e6f91e54c0c7bd876dac6ab71e46
Michael Levan <mlevan1992@gmail.com>
2020-09-28 13:25:14 -0400

    Added pointer in to specify provider value

--- octopusdeploy/provider_test.go
+++ octopusdeploy/provider_test.go
@@ -7,13 +7,13 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-var testAccProviders map[string]schema.Provider
+var testAccProviders map[string]*schema.Provider
 var testAccProvider *schema.Provider
 
 func init() {
 	testAccProvider = Provider()
-	testAccProviders = map[string]schema.Provider{
-		"octopusdeploy": *testAccProvider,
+	testAccProviders = map[string]*schema.Provider{
+		"octopusdeploy": testAccProvider,
 	}
 }
 
