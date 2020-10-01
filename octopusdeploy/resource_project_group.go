ddfe460a98b9569c66763a2227fe40d7be3c3591
AdminTurnedDevOps <mlevan1992@gmail.com>
2020-10-01 09:16:38 -0400

    Added im changes to implement the new Terraform SDK 2.0 diagnostics support. The changes include: 1) The schema.Resource CRUD operations changed. For example "Create" turned into "CreateContext". 2) A context has now been added into each function for CRUD operations. 3) The `diag.Diagnostics type has been added to each CRUD operation all with the new return value of `diags`.

--- octopusdeploy/resource_project_group.go
+++ octopusdeploy/resource_project_group.go
@@ -12,10 +12,10 @@ import (
 
 func resourceProjectGroup() *schema.Resource {
 	return &schema.Resource{
-		// Create: resourceProjectGroupCreate,
-		// Read:   resourceProjectGroupRead,
-		// Update: resourceProjectGroupUpdate,
-		// Delete: resourceProjectGroupDelete,
+		CreateContext: resourceProjectGroupCreate,
+		ReadContext:   resourceProjectGroupRead,
+		UpdateContext: resourceProjectGroupUpdate,
+		DeleteContext: resourceProjectGroupDelete,
 
 		Schema: map[string]*schema.Schema{
 			constName: {
@@ -66,14 +66,20 @@ func resourceProjectGroupCreate(ctx context.Context, d *schema.ResourceData, m i
 	return diags
 }
 
-func resourceProjectGroupRead(d *schema.ResourceData, m interface{}) error {
+func resourceProjectGroupRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	id := d.Id()
+
+	var diags diag.Diagnostics
+
+	if diags == nil {
+		log.Println("diag package is empty")
+	}
 
 	apiClient := m.(*client.Client)
 	resource, err := apiClient.ProjectGroups.GetByID(id)
 	if err != nil {
-		return createResourceOperationError(errorReadingProjectGroup, id, err)
-		// diag.Errorf(err)
+		// return createResourceOperationError(errorReadingProjectGroup, id, err)
+		diag.FromErr(err)
 	}
 	if resource == nil {
 		d.SetId(constEmptyString)
@@ -85,36 +91,48 @@ func resourceProjectGroupRead(d *schema.ResourceData, m interface{}) error {
 	d.Set(constName, resource.Name)
 	d.Set(constDescription, resource.Description)
 
-	return nil
+	return diags
 }
 
-func resourceProjectGroupUpdate(d *schema.ResourceData, m interface{}) error {
+func resourceProjectGroupUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	projectGroup := buildProjectGroupResource(d)
 	projectGroup.ID = d.Id() // set ID so Octopus API knows which project group to update
+
+	var diags diag.Diagnostics
+
+	if diags == nil {
+		log.Println("diag package is empty")
+	}
 
 	apiClient := m.(*client.Client)
 	resource, err := apiClient.ProjectGroups.Update(*projectGroup)
 	if err != nil {
-		return createResourceOperationError(errorUpdatingProjectGroup, d.Id(), err)
-		// diag.Errorf(err)
+		// return createResourceOperationError(errorUpdatingProjectGroup, d.Id(), err)
+		diag.FromErr(err)
 	}
 
 	d.SetId(resource.ID)
 
-	return nil
+	return diags
 }
 
-func resourceProjectGroupDelete(d *schema.ResourceData, m interface{}) error {
+func resourceProjectGroupDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	id := d.Id()
+
+	var diags diag.Diagnostics
+
+	if diags == nil {
+		log.Println("diag package is empty")
+	}
 
 	apiClient := m.(*client.Client)
 	err := apiClient.ProjectGroups.DeleteByID(id)
 	if err != nil {
-		return createResourceOperationError(errorDeletingProjectGroup, id, err)
-		// diag.Errorf(err)
+		// return createResourceOperationError(errorDeletingProjectGroup, id, err)
+		diag.FromErr(err)
 	}
 
 	d.SetId(constEmptyString)
 
-	return nil
+	return diags
 }
