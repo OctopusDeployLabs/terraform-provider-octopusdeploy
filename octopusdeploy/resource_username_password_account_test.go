028dabf8e5ba3269d82d34b2ee0fe2f5f56417ef
John Bristowe <john.bristowe@octopus.com>
2020-11-05 14:21:14 +1000

    Added random values

--- octopusdeploy/resource_username_password_account_test.go
+++ octopusdeploy/resource_username_password_account_test.go
@@ -7,71 +7,41 @@ import (
 	"github.com/OctopusDeploy/go-octopusdeploy/octopusdeploy"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccUsernamePasswordBasic(t *testing.T) {
-	name := acctest.RandString(10)
-	resourceName := constOctopusDeployUsernamePasswordAccount + "." + name
+	localName := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
+	prefix := constOctopusDeployUsernamePasswordAccount + "." + localName
 
-	password := acctest.RandString(10)
-	username := acctest.RandString(10)
+	name := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
+	password := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
+	tenantedDeploymentParticipation := octopusdeploy.TenantedDeploymentModeTenantedOrUntenanted
+	username := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
 
 	resource.Test(t, resource.TestCase{
+		CheckDestroy: testAccountDestroy,
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
-		CheckDestroy: testOctopusDeployUsernamePasswordDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testUsernamePasswordBasic(name, username, password),
+				Config: testUsernamePasswordBasic(localName, name, username, password, tenantedDeploymentParticipation),
 				Check: resource.ComposeTestCheckFunc(
-					testUsernamePasswordExists(name),
-					resource.TestCheckResourceAttr(resourceName, constName, name),
-					resource.TestCheckResourceAttr(resourceName, constPassword, password),
-					resource.TestCheckResourceAttr(resourceName, constUsername, username),
+					testAccountExists(prefix),
+					resource.TestCheckResourceAttr(prefix, constName, name),
+					resource.TestCheckResourceAttr(prefix, constPassword, password),
+					resource.TestCheckResourceAttr(prefix, constTenantedDeploymentParticipation, string(tenantedDeploymentParticipation)),
+					resource.TestCheckResourceAttr(prefix, constUsername, username),
 				),
 			},
 		},
 	})
 }
 
-func testUsernamePasswordBasic(name string, username string, password string) string {
-	return fmt.Sprintf(`
-		resource "%s" "%s" {
-			name = "%s"
-			password = "%s"
-			username = "%s"
-		}
-		`,
-		constOctopusDeployUsernamePasswordAccount, name, name, password, username,
-	)
-}
-
-func testUsernamePasswordExists(name string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		client := testAccProvider.Meta().(*octopusdeploy.Client)
-		accountID := s.RootModule().Resources[constOctopusDeployUsernamePasswordAccount+"."+name].Primary.ID
-		if _, err := client.Accounts.GetByID(accountID); err != nil {
-			return err
-		}
-
-		return nil
-	}
-}
-
-func testOctopusDeployUsernamePasswordDestroy(s *terraform.State) error {
-	client := testAccProvider.Meta().(*octopusdeploy.Client)
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != constOctopusDeployUsernamePasswordAccount {
-			continue
-		}
-
-		accountID := rs.Primary.ID
-		if _, err := client.Accounts.GetByID(accountID); err != nil {
-			return err
-		}
-		return fmt.Errorf("account (%s) still exists", rs.Primary.ID)
-	}
-
-	return nil
+func testUsernamePasswordBasic(localName string, name string, username string, password string, tenantedDeploymentParticipation octopusdeploy.TenantedDeploymentMode) string {
+	return fmt.Sprintf(`resource "%s" "%s" {
+		name = "%s"
+		password = "%s"
+		tenanted_deployment_participation = "%s"
+		username = "%s"
+	}`, constOctopusDeployUsernamePasswordAccount, localName, name, password, tenantedDeploymentParticipation, username)
 }
