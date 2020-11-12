594177d86ecb057257a1615908cb572117fb9c48
John Bristowe <john.bristowe@octopus.com>
2020-11-12 12:33:03 +1000

    Moved schema and fixed processing

--- octopusdeploy/data_source_channels.go
+++ octopusdeploy/data_source_channels.go
@@ -12,93 +12,7 @@ import (
 func dataSourceChannels() *schema.Resource {
 	return &schema.Resource{
 		ReadContext: dataSourceChannelsRead,
-		Schema: map[string]*schema.Schema{
-			"ids": {
-				Elem:     &schema.Schema{Type: schema.TypeString},
-				Optional: true,
-				Type:     schema.TypeList,
-			},
-			"partial_name": {
-				Optional: true,
-				Type:     schema.TypeString,
-			},
-			"skip": {
-				Default:  0,
-				Type:     schema.TypeInt,
-				Optional: true,
-			},
-			"take": {
-				Default:  1,
-				Type:     schema.TypeInt,
-				Optional: true,
-			},
-			"channels": {
-				Computed: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"description": {
-							Optional: true,
-							Type:     schema.TypeString,
-						},
-						"id": {
-							Optional: true,
-							Type:     schema.TypeString,
-						},
-						"is_default": {
-							Optional: true,
-							Type:     schema.TypeBool,
-						},
-						"lifecycle_id": {
-							Optional: true,
-							Type:     schema.TypeString,
-						},
-						"name": {
-							Optional: true,
-							Type:     schema.TypeString,
-						},
-						"space_id": {
-							Optional: true,
-							Type:     schema.TypeString,
-						},
-						"project_id": {
-							Optional: true,
-							Type:     schema.TypeString,
-						},
-						"rules": {
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"actions": {
-										Elem:     &schema.Schema{Type: schema.TypeString},
-										Optional: true,
-										Type:     schema.TypeList,
-									},
-									"id": {
-										Optional: true,
-										Type:     schema.TypeString,
-									},
-									"tag": {
-										Optional: true,
-										Type:     schema.TypeString,
-									},
-									"version_range": {
-										Optional: true,
-										Type:     schema.TypeString,
-									},
-								},
-							},
-							Optional: true,
-							Type:     schema.TypeList,
-						},
-						"tenant_tags": {
-							Type:     schema.TypeList,
-							Optional: true,
-							Elem:     &schema.Schema{Type: schema.TypeString},
-						},
-					},
-				},
-				Type: schema.TypeList,
-			},
-		},
+		Schema:      getChannelDataSchema(),
 	}
 }
 
@@ -118,18 +32,7 @@ func dataSourceChannelsRead(ctx context.Context, d *schema.ResourceData, m inter
 
 	flattenedChannels := []interface{}{}
 	for _, channel := range channels.Items {
-		flattenedChannel := map[string]interface{}{
-			"description":  channel.Description,
-			"id":           channel.GetID(),
-			"is_default":   channel.IsDefault,
-			"lifecycle_id": channel.LifecycleID,
-			"name":         channel.Name,
-			"project_id":   channel.ProjectID,
-			"rules":        flattenRules(channel.Rules),
-			"space_id":     channel.SpaceID,
-			"tenant_tags":  channel.TenantTags,
-		}
-		flattenedChannels = append(flattenedChannels, flattenedChannel)
+		flattenedChannels = append(flattenedChannels, flattenChannel(channel))
 	}
 
 	d.Set("channels", flattenedChannels)
--- octopusdeploy/schema_channel.go
+++ octopusdeploy/schema_channel.go
@@ -18,11 +18,13 @@ func expandChannel(d *schema.ResourceData) *octopusdeploy.Channel {
 
 	channel.IsDefault = d.Get("is_default").(bool)
 	channel.LifecycleID = d.Get("lifecycle_id").(string)
+	channel.SpaceID = d.Get("space_id").(string)
+	channel.TenantTags = getSliceFromTerraformTypeList(d.Get("tenant_tags"))
 
 	if v, ok := d.GetOk("rule"); ok {
 		channelRules := v.([]interface{})
 		for _, channelRule := range channelRules {
-			rule := expandRules(channelRule.(map[string]interface{}))
+			rule := expandChannelRule(channelRule.(map[string]interface{}))
 			channel.Rules = append(channel.Rules, rule)
 		}
 	}
@@ -30,7 +32,7 @@ func expandChannel(d *schema.ResourceData) *octopusdeploy.Channel {
 	return channel
 }
 
-func expandRules(channelRule map[string]interface{}) octopusdeploy.ChannelRule {
+func expandChannelRule(channelRule map[string]interface{}) octopusdeploy.ChannelRule {
 	return octopusdeploy.ChannelRule{
 		Actions:      getSliceFromTerraformTypeList(channelRule["actions"]),
 		ID:           channelRule["id"].(string),
@@ -39,18 +41,34 @@ func expandRules(channelRule map[string]interface{}) octopusdeploy.ChannelRule {
 	}
 }
 
-func flattenChannel(ctx context.Context, d *schema.ResourceData, channel *octopusdeploy.Channel) {
+func flattenChannel(channel *octopusdeploy.Channel) map[string]interface{} {
+	if channel == nil {
+		return nil
+	}
+
+	return map[string]interface{}{
+		"description":  channel.Description,
+		"id":           channel.GetID(),
+		"is_default":   channel.IsDefault,
+		"lifecycle_id": channel.LifecycleID,
+		"name":         channel.Name,
+		"project_id":   channel.ProjectID,
+		"rules":        flattenChannelRules(channel.Rules),
+		"space_id":     channel.SpaceID,
+		"tenant_tags":  channel.TenantTags,
+	}
+}
+
+func setChannel(ctx context.Context, d *schema.ResourceData, channel *octopusdeploy.Channel) {
 	d.Set("description", channel.Description)
 	d.Set("is_default", channel.IsDefault)
 	d.Set("lifecycle_id", channel.LifecycleID)
 	d.Set("name", channel.Name)
 	d.Set("project_id", channel.ProjectID)
-	d.Set("rule", flattenRules(channel.Rules))
-
-	d.SetId(channel.GetID())
+	d.Set("rules", flattenChannelRules(channel.Rules))
 }
 
-func flattenRules(channelRules []octopusdeploy.ChannelRule) []map[string]interface{} {
+func flattenChannelRules(channelRules []octopusdeploy.ChannelRule) []map[string]interface{} {
 	var flattenedRules = make([]map[string]interface{}, len(channelRules))
 	for key, channelRule := range channelRules {
 		flattenedRules[key] = map[string]interface{}{
@@ -64,10 +82,84 @@ func flattenRules(channelRules []octopusdeploy.ChannelRule) []map[string]interfa
 	return flattenedRules
 }
 
+func getChannelDataSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"ids": {
+			Elem:     &schema.Schema{Type: schema.TypeString},
+			Optional: true,
+			Type:     schema.TypeList,
+		},
+		"partial_name": {
+			Optional: true,
+			Type:     schema.TypeString,
+		},
+		"skip": {
+			Default:  0,
+			Type:     schema.TypeInt,
+			Optional: true,
+		},
+		"take": {
+			Default:  1,
+			Type:     schema.TypeInt,
+			Optional: true,
+		},
+		"channels": {
+			Computed: true,
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"description": {
+						Computed: true,
+						Type:     schema.TypeString,
+					},
+					"id": {
+						Computed: true,
+						Type:     schema.TypeString,
+					},
+					"is_default": {
+						Computed: true,
+						Type:     schema.TypeBool,
+					},
+					"lifecycle_id": {
+						Computed: true,
+						Type:     schema.TypeString,
+					},
+					"name": {
+						Computed: true,
+						Type:     schema.TypeString,
+					},
+					"project_id": {
+						Computed: true,
+						Type:     schema.TypeString,
+					},
+					"rules": {
+						Elem:     &schema.Resource{Schema: getChannelRuleSchema()},
+						Computed: true,
+						Type:     schema.TypeList,
+					},
+					"space_id": {
+						Computed: true,
+						Type:     schema.TypeString,
+					},
+					"tenant_tags": {
+						Type:     schema.TypeList,
+						Computed: true,
+						Elem:     &schema.Schema{Type: schema.TypeString},
+					},
+				},
+			},
+			Type: schema.TypeList,
+		},
+	}
+}
+
 func getChannelSchema() map[string]*schema.Schema {
 	return map[string]*schema.Schema{
 		"description": {
 			Optional: true,
+			Type:     schema.TypeString,
+		},
+		"id": {
+			Computed: true,
 			Type:     schema.TypeString,
 		},
 		"is_default": {
@@ -87,35 +179,41 @@ func getChannelSchema() map[string]*schema.Schema {
 			Required: true,
 			Type:     schema.TypeString,
 		},
-		"rule": {
-			Elem: &schema.Resource{
-				Schema: map[string]*schema.Schema{
-					"actions": {
-						Elem:     &schema.Schema{Type: schema.TypeString},
-						Optional: true,
-						Type:     schema.TypeList,
-					},
-					"id": {
-						Optional: true,
-						Type:     schema.TypeString,
-					},
-					"tag": {
-						Optional: true,
-						Type:     schema.TypeString,
-					},
-					"version_range": {
-						Optional: true,
-						Type:     schema.TypeString,
-					},
-				},
-			},
+		"rules": {
+			Elem:     &schema.Resource{Schema: getChannelRuleSchema()},
 			Optional: true,
 			Type:     schema.TypeList,
+		},
+		"space_id": {
+			Optional: true,
+			Type:     schema.TypeString,
 		},
 		"tenant_tags": {
 			Type:     schema.TypeList,
 			Optional: true,
 			Elem:     &schema.Schema{Type: schema.TypeString},
+		},
+	}
+}
+
+func getChannelRuleSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"actions": {
+			Elem:     &schema.Schema{Type: schema.TypeString},
+			Optional: true,
+			Type:     schema.TypeList,
+		},
+		"id": {
+			Optional: true,
+			Type:     schema.TypeString,
+		},
+		"tag": {
+			Optional: true,
+			Type:     schema.TypeString,
+		},
+		"version_range": {
+			Optional: true,
+			Type:     schema.TypeString,
 		},
 	}
 }
