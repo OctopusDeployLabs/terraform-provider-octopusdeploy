bbf81daf6c49f9e3d2b1faeb0d28fde802517618
John Bristowe <john.bristowe@octopus.com>
2020-11-16 06:09:13 +1000

    Added missing fields; moved schema functions into this file

--- octopusdeploy/schema_account_resource.go
+++ octopusdeploy/schema_account_resource.go
@@ -28,15 +28,6 @@ func expandAccountResource(d *schema.ResourceData) *octopusdeploy.AccountResourc
 		accountResource.AuthenticationEndpoint = v.(string)
 	}
 
-	if v, ok := d.GetOk("client_id"); ok {
-		clientID := uuid.MustParse(v.(string))
-		accountResource.ApplicationID = &clientID
-	}
-
-	if v, ok := d.GetOk("client_secret"); ok {
-		accountResource.ApplicationPassword = octopusdeploy.NewSensitiveValue(v.(string))
-	}
-
 	if v, ok := d.GetOk("azure_environment"); ok {
 		accountResource.AzureEnvironment = v.(string)
 	}
@@ -47,6 +38,19 @@ func expandAccountResource(d *schema.ResourceData) *octopusdeploy.AccountResourc
 
 	if v, ok := d.GetOk("certificate_thumbprint"); ok {
 		accountResource.CertificateThumbprint = v.(string)
+	}
+
+	if v, ok := d.GetOk("client_id"); ok {
+		clientID := uuid.MustParse(v.(string))
+		accountResource.ApplicationID = &clientID
+	}
+
+	if v, ok := d.GetOk("client_secret"); ok {
+		accountResource.ApplicationPassword = octopusdeploy.NewSensitiveValue(v.(string))
+	}
+
+	if v, ok := d.GetOk("description"); ok {
+		accountResource.Description = v.(string)
 	}
 
 	if v, ok := d.GetOk("environments"); ok {
@@ -65,12 +69,16 @@ func expandAccountResource(d *schema.ResourceData) *octopusdeploy.AccountResourc
 		accountResource.PrivateKeyFile = octopusdeploy.NewSensitiveValue(v.(string))
 	}
 
-	if v, ok := d.GetOk("service_management_endpoint_suffix"); ok {
-		accountResource.StorageEndpointSuffix = v.(string)
-	}
-
 	if v, ok := d.GetOk("resource_management_endpoint_base_uri"); ok {
 		accountResource.ResourceManagerEndpoint = v.(string)
+	}
+
+	if v, ok := d.GetOk("service_management_endpoint_base_uri"); ok {
+		accountResource.ManagementEndpoint = v.(string)
+	}
+
+	if v, ok := d.GetOk("service_management_endpoint_suffix"); ok {
+		accountResource.StorageEndpointSuffix = v.(string)
 	}
 
 	if v, ok := d.GetOk("secret_key"); ok {
@@ -81,7 +89,7 @@ func expandAccountResource(d *schema.ResourceData) *octopusdeploy.AccountResourc
 		accountResource.SpaceID = v.(string)
 	}
 
-	if v, ok := d.GetOk("subscription_number"); ok {
+	if v, ok := d.GetOk("subscription_id"); ok {
 		subscriptionID := uuid.MustParse(v.(string))
 		accountResource.SubscriptionID = &subscriptionID
 	}
@@ -114,82 +122,163 @@ func expandAccountResource(d *schema.ResourceData) *octopusdeploy.AccountResourc
 	return accountResource
 }
 
-func flattenAccountResource(ctx context.Context, d *schema.ResourceData, account *octopusdeploy.AccountResource) {
-	flattenAccount(ctx, d, account)
+func getAccountResourceSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"access_key": {
+			Optional: true,
+			Type:     schema.TypeString,
+		},
+		"account_type": {
+			Default:  "None",
+			Optional: true,
+			Type:     schema.TypeString,
+			ValidateDiagFunc: validateValueFunc([]string{
+				"AmazonWebServicesAccount",
+				"AmazonWebServicesRoleAccount",
+				"AzureServicePrincipal",
+				"AzureSubscription",
+				"None",
+				"SshKeyPair",
+				"Token",
+				"UsernamePassword",
+			}),
+		},
+		"active_directory_endpoint_base_uri": {
+			Optional: true,
+			Type:     schema.TypeString,
+		},
+		"azure_environment": {
+			Optional: true,
+			Type:     schema.TypeString,
+		},
+		"certificate_data": {
+			Optional:  true,
+			Sensitive: true,
+			Type:      schema.TypeString,
+		},
+		"certificate_thumbprint": {
+			Optional:  true,
+			Sensitive: true,
+			Type:      schema.TypeString,
+		},
+		"client_id": {
+			Optional:         true,
+			Type:             schema.TypeString,
+			ValidateDiagFunc: validateDiagFunc(validation.IsUUID),
+		},
+		"client_secret": {
+			Optional:  true,
+			Sensitive: true,
+			Type:      schema.TypeString,
+		},
+		"description": {
+			Optional: true,
+			Type:     schema.TypeString,
+		},
+		"environments": {
+			Elem:     &schema.Schema{Type: schema.TypeString},
+			Optional: true,
+			Type:     schema.TypeList,
+		},
+		"id": {
+			Computed: true,
+			Type:     schema.TypeString,
+		},
+		"name": &schema.Schema{
+			Required:     true,
+			Type:         schema.TypeString,
+			ValidateFunc: validation.StringIsNotEmpty,
+		},
+		"password": {
+			Optional:  true,
+			Sensitive: true,
+			Type:      schema.TypeString,
+		},
+		"resource_management_endpoint_base_uri": {
+			Optional: true,
+			Type:     schema.TypeString,
+		},
+		"private_key_file": {
+			Optional:  true,
+			Sensitive: true,
+			Type:      schema.TypeString,
+		},
+		"private_key_passphrase": {
+			Optional:  true,
+			Sensitive: true,
+			Type:      schema.TypeString,
+		},
+		"secret_key": {
+			Optional:  true,
+			Sensitive: true,
+			Type:      schema.TypeString,
+		},
+		"service_management_endpoint_base_uri": {
+			Optional: true,
+			Type:     schema.TypeString,
+		},
+		"service_management_endpoint_suffix": {
+			Optional: true,
+			Type:     schema.TypeString,
+		},
+		"space_id": {
+			Computed: true,
+			Type:     schema.TypeString,
+		},
+		"subscription_id": {
+			Type:             schema.TypeString,
+			Optional:         true,
+			ValidateDiagFunc: validateDiagFunc(validation.IsUUID),
+		},
+		"tenanted_deployment_participation": getTenantedDeploymentSchema(),
+		"tenants": {
+			Elem:     &schema.Schema{Type: schema.TypeString},
+			Optional: true,
+			Type:     schema.TypeList,
+		},
+		"tenant_id": {
+			Type:             schema.TypeString,
+			Required:         true,
+			ValidateDiagFunc: validateDiagFunc(validation.IsUUID),
+		},
+		"tenant_tags": {
+			Elem:     &schema.Schema{Type: schema.TypeString},
+			Optional: true,
+			Type:     schema.TypeList,
+		},
+		"token": {
+			Optional:  true,
+			Sensitive: true,
+			Type:      schema.TypeString,
+		},
+		"username": {
+			Optional: true,
+			Type:     schema.TypeString,
+		},
+	}
+}
 
+func setAccountResource(ctx context.Context, d *schema.ResourceData, account *octopusdeploy.AccountResource) {
 	d.Set("access_key", account.AccessKey)
+	d.Set("account_type", account.GetAccountType())
 	d.Set("active_directory_endpoint_base_uri", account.AuthenticationEndpoint)
 	d.Set("azure_environment", account.AzureEnvironment)
-	d.Set("certificate_bytes", account.CertificateBytes)
 	d.Set("certificate_thumbprint", account.CertificateThumbprint)
 	d.Set("client_id", account.ApplicationID.String())
-	d.Set("client_secret", account.ApplicationPassword)
-	d.Set("service_management_endpoint_base_uri", account.ManagementEndpoint)
-	d.Set("password", account.ApplicationPassword)
-	d.Set("private_key_file", account.PrivateKeyFile)
-	d.Set("private_key_passphrase", account.PrivateKeyPassphrase)
+	d.Set("description", account.GetDescription())
+	d.Set("environments", account.GetEnvironmentIDs())
+	d.Set("name", account.GetName())
 	d.Set("resource_management_endpoint_base_uri", account.ResourceManagerEndpoint)
-	d.Set("service_management_endpoint_suffix", account.StorageEndpointSuffix)
 	d.Set("secret_key", account.SecretKey)
-	d.Set("subscription_number", account.SubscriptionID.String())
+	d.Set("service_management_endpoint_base_uri", account.ManagementEndpoint)
+	d.Set("service_management_endpoint_suffix", account.StorageEndpointSuffix)
+	d.Set("space_id", account.GetSpaceID())
+	d.Set("subscription_id", account.SubscriptionID.String())
+	d.Set("tenanted_deployment_participation", account.GetTenantedDeploymentMode())
+	d.Set("tenants", account.GetTenantIDs())
 	d.Set("tenant_id", account.TenantID.String())
-	d.Set("token", account.Token.NewValue)
+	d.Set("tenant_tags", account.GetTenantTags())
 	d.Set("username", account.Username)
 
 	d.SetId(account.GetID())
-}
-
-func getAccountResourceSchema() map[string]*schema.Schema {
-	schemaMap := getAccountSchema()
-	schemaMap["access_key"] = &schema.Schema{
-		Optional: true,
-		Type:     schema.TypeString,
-	}
-	schemaMap["active_directory_endpoint_base_uri"] = &schema.Schema{
-		Optional: true,
-		Type:     schema.TypeString,
-	}
-	schemaMap["azure_environment"] = &schema.Schema{
-		Optional: true,
-		Type:     schema.TypeString,
-	}
-	schemaMap["certificate_bytes"] = &schema.Schema{
-		Optional:  true,
-		Sensitive: true,
-		Type:      schema.TypeString,
-	}
-	schemaMap["certificate_thumbprint"] = &schema.Schema{
-		Optional:  true,
-		Sensitive: true,
-		Type:      schema.TypeString,
-	}
-	schemaMap["client_id"] = &schema.Schema{
-		Optional:         true,
-		Type:             schema.TypeString,
-		ValidateDiagFunc: validateDiagFunc(validation.IsUUID),
-	}
-	schemaMap["password"] = &schema.Schema{
-		Optional:  true,
-		Sensitive: true,
-		Type:      schema.TypeString,
-	}
-	schemaMap["resource_management_endpoint_base_uri"] = &schema.Schema{
-		Optional: true,
-		Type:     schema.TypeString,
-	}
-	schemaMap["secret_key"] = &schema.Schema{
-		Optional:  true,
-		Sensitive: true,
-		Type:      schema.TypeString,
-	}
-	schemaMap["subscription_number"] = &schema.Schema{
-		Type:             schema.TypeString,
-		Optional:         true,
-		ValidateDiagFunc: validateDiagFunc(validation.IsUUID),
-	}
-	schemaMap["tenant_id"] = &schema.Schema{
-		Type:     schema.TypeString,
-		Required: true,
-	}
-	return schemaMap
 }
