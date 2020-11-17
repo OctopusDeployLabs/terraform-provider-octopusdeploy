5fa29a08cacb59419d63a019942f201de4bfa16c
John Bristowe <john.bristowe@octopus.com>
2020-11-17 18:50:38 +1000

    Updated formatting

--- examples/AWS-Account/main.tf
+++ examples/AWS-Account/main.tf
@@ -1,11 +1,11 @@
 provider "octopusdeploy" {
-    address = var.serverURL
-    apikey  = var.apiKey
-    space_id   = var.space
+  address  = var.serverURL
+  apikey   = var.apiKey
+  space_id = var.space
 }
 
 resource "octopusdeploy_aws_account" "aw" {
-    name = "awsaccount"
-    access_key = var.accessKey
-    secret_key = var.secretKey
+  name       = "awsaccount"
+  access_key = var.accessKey
+  secret_key = var.secretKey
 }
