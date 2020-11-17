454a79141e2740188221c286d71a4111a36db808
John Bristowe <john.bristowe@octopus.com>
2020-11-17 15:41:51 +1000

    Removed unused functions

--- octopusdeploy/schema_account.go
+++ octopusdeploy/schema_account.go
@@ -1,11 +1,8 @@
 package octopusdeploy
 
 import (
-	"context"
-
 	"github.com/OctopusDeploy/go-octopusdeploy/octopusdeploy"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
 
 func flattenAccount(accountResource *octopusdeploy.AccountResource) map[string]interface{} {
@@ -156,67 +153,4 @@ func getAccountDataSchema() map[string]*schema.Schema {
 			Type: schema.TypeList,
 		},
 	}
-}
-
-func getAccountSchema() map[string]*schema.Schema {
-	return map[string]*schema.Schema{
-		"account_type": {
-			Default:  "None",
-			Optional: true,
-			Type:     schema.TypeString,
-			ValidateDiagFunc: validateValueFunc([]string{
-				"AmazonWebServicesAccount",
-				"AzureServicePrincipal",
-				"AzureSubscription",
-				"None",
-				"SshKeyPair",
-				"Token",
-				"UsernamePassword",
-			}),
-		},
-		"description": {
-			Optional: true,
-			Type:     schema.TypeString,
-		},
-		"environments": {
-			Elem:     &schema.Schema{Type: schema.TypeString},
-			Optional: true,
-			Type:     schema.TypeList,
-		},
-		"id": {
-			Computed: true,
-			Type:     schema.TypeString,
-		},
-		"name": &schema.Schema{
-			Required:     true,
-			Type:         schema.TypeString,
-			ValidateFunc: validation.StringIsNotEmpty,
-		},
-		"space_id": {
-			Computed: true,
-			Type:     schema.TypeString,
-		},
-		"tenanted_deployment_participation": getTenantedDeploymentSchema(),
-		"tenants": {
-			Elem:     &schema.Schema{Type: schema.TypeString},
-			Optional: true,
-			Type:     schema.TypeList,
-		},
-		"tenant_tags": {
-			Elem:     &schema.Schema{Type: schema.TypeString},
-			Optional: true,
-			Type:     schema.TypeList,
-		},
-	}
-}
-
-func setAccount(ctx context.Context, d *schema.ResourceData, account octopusdeploy.IAccount) {
-	d.Set("account_type", account.GetAccountType())
-	d.Set("description", account.GetDescription())
-	d.Set("environments", account.GetEnvironmentIDs())
-	d.Set("name", account.GetName())
-	d.Set("space_id", account.GetSpaceID())
-	d.Set("tenanted_deployment_participation", account.GetTenantedDeploymentMode())
-	d.Set("tenants", account.GetTenantIDs())
-	d.Set("tenant_tags", account.GetTenantTags())
 }
