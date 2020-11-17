7378873efd192f1c18d3b5c232a23d19e47d422c
John Bristowe <john.bristowe@octopus.com>
2020-11-17 18:51:11 +1000

    Updated formatting

--- examples/Certificate-Creation/main.tf
+++ examples/Certificate-Creation/main.tf
@@ -1,11 +1,10 @@
 provider "octopusdeploy" {
-  address = var.serverURL
-  apikey  = var.apiKey
-  space_id   = var.space
+  address  = var.serverURL
+  apikey   = var.apiKey
+  space_id = var.space
 }
 
-resource "octopusdeploy_certificate" "DevCert" {
-  name = "DevCert"
-
+resource "octopusdeploy_certificate" "developer_certificate" {
   certificate_data = var.certEOM
+  name             = "developer certificate"
 }
