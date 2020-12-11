fc3d1fc3492267d3ef9457584f1a91e707de407d
jbristowe <john.bristowe@octopus.com>
2020-12-11 13:09:25 +1000

    Added logging and applied code formatting

--- octopusdeploy/resource_username_password_account.go
+++ octopusdeploy/resource_username_password_account.go
@@ -2,6 +2,7 @@ package octopusdeploy
 
 import (
 	"context"
+	"log"
 
 	"github.com/OctopusDeploy/go-octopusdeploy/octopusdeploy"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -12,6 +13,7 @@ func resourceUsernamePasswordAccount() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: resourceUsernamePasswordAccountCreate,
 		DeleteContext: resourceUsernamePasswordAccountDelete,
+		Description:   "This resource manages username-password accounts in Octopus Deploy.",
 		Importer:      getImporter(),
 		ReadContext:   resourceUsernamePasswordAccountRead,
 		Schema:        getUsernamePasswordAccountSchema(),
@@ -22,35 +24,47 @@ func resourceUsernamePasswordAccount() *schema.Resource {
 func resourceUsernamePasswordAccountCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	account := expandUsernamePasswordAccount(d)
 
+	log.Printf("[INFO] creating username-password account: %#v", account)
+
 	client := m.(*octopusdeploy.Client)
 	createdAccount, err := client.Accounts.Add(account)
 	if err != nil {
 		diag.FromErr(err)
 	}
 
-	createdUsernamePasswordAccount := createdAccount.(*octopusdeploy.UsernamePasswordAccount)
+	if err := setUsernamePasswordAccount(ctx, d, createdAccount.(*octopusdeploy.UsernamePasswordAccount)); err != nil {
+		return diag.FromErr(err)
+	}
 
-	setUsernamePasswordAccount(ctx, d, createdUsernamePasswordAccount)
+	d.SetId(createdAccount.GetID())
+
+	log.Printf("[INFO] username-password account created (%s)", d.Id())
 	return nil
 }
 
 func resourceUsernamePasswordAccountDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Printf("[INFO] deleting username-password account (%s)", d.Id())
+
 	client := m.(*octopusdeploy.Client)
-	err := client.Accounts.DeleteByID(d.Id())
-	if err != nil {
+	if err := client.Accounts.DeleteByID(d.Id()); err != nil {
 		return diag.FromErr(err)
 	}
 
 	d.SetId("")
+
+	log.Printf("[INFO] username-password account deleted")
 	return nil
 }
 
 func resourceUsernamePasswordAccountRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Printf("[INFO] reading username-password account (%s)", d.Id())
+
 	client := m.(*octopusdeploy.Client)
 	accountResource, err := client.Accounts.GetByID(d.Id())
 	if err != nil {
 		apiError := err.(*octopusdeploy.APIError)
 		if apiError.StatusCode == 404 {
+			log.Printf("[INFO] username-password account (%s) not found; deleting from state", d.Id())
 			d.SetId("")
 			return nil
 		}
@@ -64,12 +78,18 @@ func resourceUsernamePasswordAccountRead(ctx context.Context, d *schema.Resource
 
 	usernamePasswordAccount := accountResource.(*octopusdeploy.UsernamePasswordAccount)
 
-	setUsernamePasswordAccount(ctx, d, usernamePasswordAccount)
+	if err := setUsernamePasswordAccount(ctx, d, usernamePasswordAccount); err != nil {
+		return diag.FromErr(err)
+	}
+
+	log.Printf("[INFO] username-password account read: %#v", usernamePasswordAccount)
 	return nil
 }
 
 func resourceUsernamePasswordAccountUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	account := expandUsernamePasswordAccount(d)
+
+	log.Printf("[INFO] updating username-password account: %#v", account)
 
 	client := m.(*octopusdeploy.Client)
 	accountResource, err := client.Accounts.Update(account)
@@ -82,8 +102,10 @@ func resourceUsernamePasswordAccountUpdate(ctx context.Context, d *schema.Resour
 		return diag.FromErr(err)
 	}
 
-	updatedUsernamePasswordAccount := accountResource.(*octopusdeploy.UsernamePasswordAccount)
+	if err := setUsernamePasswordAccount(ctx, d, accountResource.(*octopusdeploy.UsernamePasswordAccount)); err != nil {
+		return diag.FromErr(err)
+	}
 
-	setUsernamePasswordAccount(ctx, d, updatedUsernamePasswordAccount)
+	log.Printf("[INFO] username-password account updated (%s)", d.Id())
 	return nil
 }
