d5e534943b3087889aa445973dd6479db6d50dbe
jbristowe <john.bristowe@octopus.com>
2021-01-28 12:03:53 +1000

    Update schema_azure_web_app.go
    Fixes #122

--- octopusdeploy/schema_azure_web_app.go
+++ octopusdeploy/schema_azure_web_app.go
@@ -7,6 +7,7 @@ import (
 
 func expandAzureWebApp(flattenedMap map[string]interface{}) *octopusdeploy.AzureWebAppEndpoint {
 	endpoint := octopusdeploy.NewAzureWebAppEndpoint()
+	endpoint.AccountID = flattenedMap["account_id"].(string)
 	endpoint.ID = flattenedMap["id"].(string)
 	endpoint.ResourceGroupName = flattenedMap["resource_group_name"].(string)
 	endpoint.WebAppName = flattenedMap["web_app_name"].(string)
@@ -21,6 +22,7 @@ func flattenAzureWebApp(endpoint *octopusdeploy.AzureWebAppEndpoint) []interface
 	}
 
 	return []interface{}{map[string]interface{}{
+		"account_id":          endpoint.AccountID,
 		"id":                  endpoint.GetID(),
 		"resource_group_name": endpoint.ResourceGroupName,
 		"web_app_name":        endpoint.WebAppName,
@@ -30,6 +32,10 @@ func flattenAzureWebApp(endpoint *octopusdeploy.AzureWebAppEndpoint) []interface
 
 func getAzureWebAppSchema() map[string]*schema.Schema {
 	return map[string]*schema.Schema{
+		"account_id": {
+			Optional: true,
+			Type:     schema.TypeString,
+		},
 		"id": getIDSchema(),
 		"resource_group_name": {
 			Optional: true,
