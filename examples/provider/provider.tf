b000d67e6fcd96050b40a784383f53782930fe8c
jbristowe <john.bristowe@octopus.com>
2021-01-30 06:52:48 +1000

    Added provider configuration example

---
+++ examples/provider/provider.tf
@@ -0,0 +1,6 @@
+provider "octopusdeploy" {
+  address    = "https://octopus.example.com"
+  api_key    = "API-XXXXXXXXXXXXX"
+  space_id   = "Spaces-321" # the ID of the space
+  space_name = "Support" # the name of the space
+}
