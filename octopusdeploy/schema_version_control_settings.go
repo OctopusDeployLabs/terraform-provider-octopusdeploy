5e416ba80c6d3e2e23e401c21bdbeb53661c58ac
jbristowe <john.bristowe@octopus.com>
2021-03-12 11:37:07 +1000

    Updated to reflect underlying schema

--- octopusdeploy/schema_version_control_settings.go
+++ octopusdeploy/schema_version_control_settings.go
@@ -11,7 +11,9 @@ func expandVersionControlSettings(versionControlSettings interface{}) *octopusde
 	versionControlSettingsMap := versionControlSettingsList[0].(map[string]interface{})
 
 	return &octopusdeploy.VersionControlSettings{
+		BasePath:      versionControlSettingsMap["base_path"].(string),
 		DefaultBranch: versionControlSettingsMap["default_branch"].(string),
+		HasValue:      versionControlSettingsMap["has_value"].(bool),
 		Password:      octopusdeploy.NewSensitiveValue(versionControlSettingsMap["password"].(string)),
 		URL:           versionControlSettingsMap["url"].(string),
 		Username:      versionControlSettingsMap["username"].(string),
@@ -24,7 +26,9 @@ func flattenVersionControlSettings(versionControlSettings *octopusdeploy.Version
 	}
 
 	flattenedVersionControlSettings := make(map[string]interface{})
+	flattenedVersionControlSettings["base_path"] = versionControlSettings.BasePath
 	flattenedVersionControlSettings["default_branch"] = versionControlSettings.DefaultBranch
+	flattenedVersionControlSettings["has_value"] = versionControlSettings.HasValue
 	flattenedVersionControlSettings["url"] = versionControlSettings.URL
 	flattenedVersionControlSettings["username"] = versionControlSettings.Username
 	return []interface{}{flattenedVersionControlSettings}
@@ -32,10 +36,19 @@ func flattenVersionControlSettings(versionControlSettings *octopusdeploy.Version
 
 func getVersionControlSettingsSchema() map[string]*schema.Schema {
 	return map[string]*schema.Schema{
+		"base_path": {
+			Description: "The base path associated with these version control settings.",
+			Type:        schema.TypeString,
+			Optional:    true,
+		},
 		"default_branch": {
 			Description: "The default branch associated with these version control settings.",
 			Type:        schema.TypeString,
 			Optional:    true,
+		},
+		"has_value": {
+			Type:     schema.TypeBool,
+			Optional: true,
 		},
 		"password": {
 			Description:      "The password associated with these version control settings.",
