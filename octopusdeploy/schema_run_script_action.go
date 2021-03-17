4620212de3ffffe868797bcb3f1edba24924acdf
jbristowe <john.bristowe@octopus.com>
2021-03-17 16:11:04 +1000

    Updated parameter and variable names

--- octopusdeploy/schema_run_script_action.go
+++ octopusdeploy/schema_run_script_action.go
@@ -7,21 +7,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func getRunScriptActionSchema() *schema.Schema {
-	actionSchema, element := getCommonDeploymentActionSchema()
-	addExecutionLocationSchema(element)
-	addScriptFromPackageSchema(element)
-	addPackagesSchema(element, false)
-
-	element.Schema["variable_substitution_in_files"] = &schema.Schema{
-		Description: "A newline-separated list of file names to transform, relative to the package contents. Extended wildcard syntax is supported.",
-		Optional:    true,
-		Type:        schema.TypeString,
-	}
-
-	return actionSchema
-}
-
 func addScriptFromPackageSchema(element *schema.Resource) {
 	element.Schema["script_file_name"] = &schema.Schema{
 		Description: "The script file name in the package",
@@ -40,92 +25,107 @@ func addScriptFromPackageSchema(element *schema.Resource) {
 	}
 }
 
-func expandRunScriptAction(flattenedRunScriptAction map[string]interface{}) octopusdeploy.DeploymentAction {
-	deploymentAction := expandDeploymentAction(flattenedRunScriptAction)
-	deploymentAction.ActionType = "Octopus.Script"
-	deploymentAction.Properties = merge(deploymentAction.Properties, flattenRunScriptActionProperties(deploymentAction))
+func expandRunScriptAction(flattenedAction map[string]interface{}) octopusdeploy.DeploymentAction {
+	action := expandDeploymentAction(flattenedAction)
+	action.ActionType = "Octopus.Script"
+	action.Properties = merge(action.Properties, flattenRunScriptActionProperties(action))
 
-	if v, ok := flattenedRunScriptAction["run_on_server"]; ok {
-		deploymentAction.Properties["Octopus.Action.RunOnServer"] = strconv.FormatBool(v.(bool))
+	if v, ok := flattenedAction["run_on_server"]; ok {
+		action.Properties["Octopus.Action.RunOnServer"] = strconv.FormatBool(v.(bool))
 	}
 
-	if v, ok := flattenedRunScriptAction["script_file_name"]; ok {
+	if v, ok := flattenedAction["script_file_name"]; ok {
 		if scriptFileName := v.(string); len(scriptFileName) > 0 {
-			deploymentAction.Properties["Octopus.Action.Script.ScriptFileName"] = scriptFileName
+			action.Properties["Octopus.Action.Script.ScriptFileName"] = scriptFileName
 		}
 	}
 
-	if v, ok := flattenedRunScriptAction["script_parameters"]; ok {
+	if v, ok := flattenedAction["script_parameters"]; ok {
 		if scriptParameters := v.(string); len(scriptParameters) > 0 {
-			deploymentAction.Properties["Octopus.Action.Script.ScriptParameters"] = scriptParameters
+			action.Properties["Octopus.Action.Script.ScriptParameters"] = scriptParameters
 		}
 	}
 
-	if v, ok := flattenedRunScriptAction["script_source"]; ok {
+	if v, ok := flattenedAction["script_source"]; ok {
 		if scriptSource := v.(string); len(scriptSource) > 0 {
-			deploymentAction.Properties["Octopus.Action.Script.ScriptSource"] = scriptSource
+			action.Properties["Octopus.Action.Script.ScriptSource"] = scriptSource
 		}
 	}
 
-	if variableSubstitutionInFiles, ok := flattenedRunScriptAction["variable_substitution_in_files"]; ok {
-		deploymentAction.Properties["Octopus.Action.SubstituteInFiles.TargetFiles"] = variableSubstitutionInFiles.(string)
-		deploymentAction.Properties["Octopus.Action.SubstituteInFiles.Enabled"] = "True"
+	if variableSubstitutionInFiles, ok := flattenedAction["variable_substitution_in_files"]; ok {
+		action.Properties["Octopus.Action.SubstituteInFiles.TargetFiles"] = variableSubstitutionInFiles.(string)
+		action.Properties["Octopus.Action.SubstituteInFiles.Enabled"] = "True"
 
-		if len(deploymentAction.Properties["Octopus.Action.EnabledFeatures"]) == 0 {
-			deploymentAction.Properties["Octopus.Action.EnabledFeatures"] = "Octopus.Features.SubstituteInFiles"
+		if len(action.Properties["Octopus.Action.EnabledFeatures"]) == 0 {
+			action.Properties["Octopus.Action.EnabledFeatures"] = "Octopus.Features.SubstituteInFiles"
 		} else {
-			deploymentAction.Properties["Octopus.Action.EnabledFeatures"] += ",Octopus.Features.SubstituteInFiles"
+			action.Properties["Octopus.Action.EnabledFeatures"] += ",Octopus.Features.SubstituteInFiles"
 		}
 	}
 
-	return deploymentAction
+	return action
 }
 
-func flattenRunScriptAction(deploymentAction octopusdeploy.DeploymentAction) map[string]interface{} {
-	flattenedRunScriptAction := flattenCommonDeploymentAction(deploymentAction)
+func flattenRunScriptAction(action octopusdeploy.DeploymentAction) map[string]interface{} {
+	flattenedAction := flattenCommonDeploymentAction(action)
 
-	if v, ok := deploymentAction.Properties["Octopus.Action.RunOnServer"]; ok {
+	if v, ok := action.Properties["Octopus.Action.RunOnServer"]; ok {
 		runOnServer, _ := strconv.ParseBool(v)
-		flattenedRunScriptAction["run_on_server"] = runOnServer
+		flattenedAction["run_on_server"] = runOnServer
 	}
 
-	if v, ok := deploymentAction.Properties["Octopus.Action.Script.ScriptFileName"]; ok {
-		flattenedRunScriptAction["script_file_name"] = v
+	if v, ok := action.Properties["Octopus.Action.Script.ScriptFileName"]; ok {
+		flattenedAction["script_file_name"] = v
 	}
 
-	if v, ok := deploymentAction.Properties["Octopus.Action.Script.ScriptParameters"]; ok {
-		flattenedRunScriptAction["script_parameters"] = v
+	if v, ok := action.Properties["Octopus.Action.Script.ScriptParameters"]; ok {
+		flattenedAction["script_parameters"] = v
 	}
 
-	if v, ok := deploymentAction.Properties["Octopus.Action.Script.ScriptSource"]; ok {
-		flattenedRunScriptAction["script_source"] = v
+	if v, ok := action.Properties["Octopus.Action.Script.ScriptSource"]; ok {
+		flattenedAction["script_source"] = v
 	}
 
-	if v, ok := deploymentAction.Properties["Octopus.Action.SubstituteInFiles.TargetFiles"]; ok {
-		flattenedRunScriptAction["variable_substitution_in_files"] = v
+	if v, ok := action.Properties["Octopus.Action.SubstituteInFiles.TargetFiles"]; ok {
+		flattenedAction["variable_substitution_in_files"] = v
 	}
 
-	return flattenedRunScriptAction
+	return flattenedAction
 }
 
-func flattenRunScriptActionProperties(deploymentAction octopusdeploy.DeploymentAction) map[string]string {
-	flattenedRunScriptActionProperties := map[string]string{}
+func flattenRunScriptActionProperties(action octopusdeploy.DeploymentAction) map[string]string {
+	flattenedProperties := map[string]string{}
 
-	if runOnServer, ok := deploymentAction.Properties["Octopus.Action.RunOnServer"]; ok {
-		flattenedRunScriptActionProperties["Octopus.Action.RunOnServer"] = runOnServer
+	if runOnServer, ok := action.Properties["Octopus.Action.RunOnServer"]; ok {
+		flattenedProperties["Octopus.Action.RunOnServer"] = runOnServer
 	}
 
-	if scriptFileName, ok := deploymentAction.Properties["Octopus.Action.Script.ScriptFileName"]; ok {
-		flattenedRunScriptActionProperties["Octopus.Action.Script.ScriptFileName"] = scriptFileName
+	if scriptFileName, ok := action.Properties["Octopus.Action.Script.ScriptFileName"]; ok {
+		flattenedProperties["Octopus.Action.Script.ScriptFileName"] = scriptFileName
 	}
 
-	if scriptParameters, ok := deploymentAction.Properties["Octopus.Action.Script.ScriptParameters"]; ok {
-		flattenedRunScriptActionProperties["Octopus.Action.Script.ScriptParameters"] = scriptParameters
+	if scriptParameters, ok := action.Properties["Octopus.Action.Script.ScriptParameters"]; ok {
+		flattenedProperties["Octopus.Action.Script.ScriptParameters"] = scriptParameters
 	}
 
-	if scriptSource, ok := deploymentAction.Properties["Octopus.Action.Script.ScriptSource"]; ok {
-		flattenedRunScriptActionProperties["Octopus.Action.Script.ScriptSource"] = scriptSource
+	if scriptSource, ok := action.Properties["Octopus.Action.Script.ScriptSource"]; ok {
+		flattenedProperties["Octopus.Action.Script.ScriptSource"] = scriptSource
 	}
 
-	return flattenedRunScriptActionProperties
+	return flattenedProperties
+}
+
+func getRunScriptActionSchema() *schema.Schema {
+	actionSchema, element := getCommonDeploymentActionSchema()
+	addExecutionLocationSchema(element)
+	addScriptFromPackageSchema(element)
+	addPackagesSchema(element, false)
+
+	element.Schema["variable_substitution_in_files"] = &schema.Schema{
+		Description: "A newline-separated list of file names to transform, relative to the package contents. Extended wildcard syntax is supported.",
+		Optional:    true,
+		Type:        schema.TypeString,
+	}
+
+	return actionSchema
 }
