cac96aa9f0ef5e869b1806ac721054f3b1fba727
jbristowe <john.bristowe@octopus.com>
2021-04-10 08:12:00 +1000

    chore: added terraform integration back

--- octopusdeploy/apply_terraform_template_action_test.go
+++ octopusdeploy/apply_terraform_template_action_test.go
@@ -1,91 +1,118 @@
 package octopusdeploy
 
-// import (
-// 	"fmt"
-// 	"strconv"
-// 	"testing"
+import (
+	"fmt"
+	"strconv"
+	"testing"
 
-// 	"github.com/OctopusDeploy/go-octopusdeploy/octopusdeploy"
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-// )
+	"github.com/OctopusDeploy/go-octopusdeploy/octopusdeploy"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
 
-// func TestAccOctopusDeployApplyTerraformAction(t *testing.T) {
-// 	additionalInitialParameters := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
-// 	allowPluginDownloads := acctest.RandIntRange(0, 2) == 0
+func TestAccOctopusDeployApplyTerraformAction(t *testing.T) {
+	allowPluginDownloads := acctest.RandIntRange(0, 2) == 0
+	applyParameters := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	initParameters := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	name := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	pluginCacheDirectory := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	runOnServer := acctest.RandIntRange(0, 2) == 0
+	workspace := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
 
-// 	resource.Test(t, resource.TestCase{
-// 		PreCheck:     func() { testAccPreCheck(t) },
-// 		Providers:    testAccProviders,
-// 		CheckDestroy: testAccCheckOctopusDeployDeploymentProcessDestroy,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Check: resource.ComposeTestCheckFunc(
-// 					testAccCheckApplyTerraformAction(additionalInitialParameters, allowPluginDownloads),
-// 				),
-// 				Config: testAccApplyTerraformAction(additionalInitialParameters, allowPluginDownloads),
-// 			},
-// 		},
-// 	})
-// }
+	scriptSource := "Inline"
+	if acctest.RandIntRange(0, 2) == 0 {
+		scriptSource = "Package"
+	}
 
-// func testAccApplyTerraformAction(additionalInitParams string, allowPluginDownloads bool) string {
-// 	return testAccBuildTestAction(fmt.Sprintf(`
-// 		apply_terraform_template_action {
-// 			name = "%s"
-// 			run_on_server = %v
+	parameters := ""
+	source := ""
+	if scriptSource == "Inline" {
+		variableName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+		variableValue := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+		source = fmt.Sprintf(`variable \"%s\" { type = string }`, variableName)
+		parameters = fmt.Sprintf(`{\"%s\":\"%s\"}`, variableName, variableValue)
+	}
 
-// 			template {
-// 				template_source = "%s"
-// 				template_parameters = "%s"
-// 			}
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckOctopusDeployDeploymentProcessDestroy,
+		Steps: []resource.TestStep{
+			{
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckApplyTerraformAction(name, runOnServer, scriptSource, allowPluginDownloads, applyParameters, initParameters, pluginCacheDirectory, workspace, source, parameters),
+				),
+				Config: testAccApplyTerraformAction(name, runOnServer, scriptSource, allowPluginDownloads, applyParameters, initParameters, pluginCacheDirectory, workspace, source, parameters),
+			},
+		},
+	})
+}
 
-// 			managed_accounts {
-// 				enable_aws_account_integration = %v
-// 				enable_azure_account_integration = %v
-// 				managed_account = "%s"
-// 			}
+func testAccApplyTerraformAction(name string, runOnServer bool, templateSource string, allowPluginDownloads bool, applyParameters string, initParameters string, pluginCacheDirectory string, workspace string, template string, templateParameters string) string {
+	return testAccBuildTestAction(fmt.Sprintf(`
+		apply_terraform_template_action {
+			name = "%s"
+			run_on_server = %v
+			template = "%s"
+			template_parameters = "%s"
+			template_source = "%s"
 
-// 			advanced_options {
-// 				allow_additional_plugin_downloads = %v
-// 				apply_parameters = "%s"
-// 				init_parameters = "%s"
-// 				plugin_cache_directory = "%s"
-// 				run_automatic_file_substitution = %v
-// 				workspace = "%s"
-// 			}
+			advanced_options {
+				allow_additional_plugin_downloads = %v
+				apply_parameters = "%s"
+				init_parameters = "%s"
+				plugin_cache_directory = "%s"
+				workspace = "%s"
+			}
 
-// 			primary_package {
-// 				package_id = "MyPackage"
-// 				feed_id = "feeds-builtin"
-// 			}
-//     }`, additionalInitParams, allowPluginDownloads, enableAwsAccountIntegration, enableAzureAccountIntegration, managedAccount, name, pluginsDirectory, runAutomaticFileSubstitution, runOnServer, terraformTemplate, terraformTemplateParameters, terraformWorkspace))
-// }
+			aws_account {
+				region = "us-east-1"
+				variable = "foo"
+				use_instance_role = true
 
-// func testAccCheckApplyTerraformAction(additionalInitialParameters string, allowPluginDownloads bool) resource.TestCheckFunc {
-// 	return func(s *terraform.State) error {
-// 		client := testAccProvider.Meta().(*octopusdeploy.Client)
+				role {
+					arn = "arn"
+					external_id = "external-id"
+					role_session_name = "role-session-name"
+					session_duration = 1800
+				}
+			}
 
-// 		process, err := getDeploymentProcess(s, client)
-// 		if err != nil {
-// 			return err
-// 		}
+			azure_account {
+				variable = "qwe"
+			}
 
-// 		action := process.Steps[0].Actions[0]
+			primary_package {
+				package_id = "MyPackage"
+				feed_id = "feeds-builtin"
+			}
+    }`, name, runOnServer, template, templateParameters, templateSource, allowPluginDownloads, applyParameters, initParameters, pluginCacheDirectory, workspace))
+}
 
-// 		if action.ActionType != "Octopus.TerraformApply" {
-// 			return fmt.Errorf("Action type is incorrect: %s", action.ActionType)
-// 		}
+func testAccCheckApplyTerraformAction(name string, runOnServer bool, scriptSource string, allowPluginDownloads bool, applyParameters string, initParameters string, pluginCacheDirectory string, workspace string, source string, parameters string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*octopusdeploy.Client)
 
-// 		if action.Properties["Octopus.Action.Terraform.AdditionalInitParams"].Value != additionalInitialParameters {
-// 			return fmt.Errorf("AdditionalInitParams: %s", action.Properties["Octopus.Action.Terraform.AdditionalInitParams"].Value)
-// 		}
+		process, err := getDeploymentProcess(s, client)
+		if err != nil {
+			return err
+		}
 
-// 		if v, _ := strconv.ParseBool(action.Properties["Octopus.Action.Terraform.AllowPluginDownloads"].Value); v != allowPluginDownloads {
-// 			return fmt.Errorf("AllowPluginDownloads: %s", action.Properties["Octopus.Action.Terraform.AllowPluginDownloads"].Value)
-// 		}
+		action := process.Steps[0].Actions[0]
 
-// 		return nil
-// 	}
-// }
+		if action.ActionType != "Octopus.TerraformApply" {
+			return fmt.Errorf("Action type is incorrect: %s", action.ActionType)
+		}
+
+		if action.Properties["Octopus.Action.Terraform.AdditionalInitParams"].Value != initParameters {
+			return fmt.Errorf("AdditionalInitParams: %s", action.Properties["Octopus.Action.Terraform.AdditionalInitParams"].Value)
+		}
+
+		if v, _ := strconv.ParseBool(action.Properties["Octopus.Action.Terraform.AllowPluginDownloads"].Value); v != allowPluginDownloads {
+			return fmt.Errorf("AllowPluginDownloads: %s", action.Properties["Octopus.Action.Terraform.AllowPluginDownloads"].Value)
+		}
+
+		return nil
+	}
+}
--- octopusdeploy/schema_apply_terraform_template_action.go
+++ octopusdeploy/schema_apply_terraform_template_action.go
@@ -7,45 +7,341 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+func addTerraformTemplateAdvancedOptionsSchema(element *schema.Resource) {
+	element.Schema["advanced_options"] = &schema.Schema{
+		Description: "Optional advanced options for Terraform",
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"allow_additional_plugin_downloads": {
+					Default:  true,
+					Optional: true,
+					Type:     schema.TypeBool,
+				},
+				"apply_parameters": {
+					Optional: true,
+					Type:     schema.TypeString,
+				},
+				"init_parameters": {
+					Optional: true,
+					Type:     schema.TypeString,
+				},
+				"plugin_cache_directory": {
+					Optional: true,
+					Type:     schema.TypeString,
+				},
+				"workspace": {
+					Optional: true,
+					Type:     schema.TypeString,
+				},
+			},
+		},
+		MaxItems: 1,
+		Required: true,
+		Type:     schema.TypeSet,
+	}
+}
+
+func addTerraformTemplateAwsAccountSchema(element *schema.Resource) {
+	element.Schema["aws_account"] = &schema.Schema{
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"region": {
+					Optional: true,
+					Type:     schema.TypeString,
+				},
+				"role": {
+					Elem: &schema.Resource{
+						Schema: map[string]*schema.Schema{
+							"arn": {
+								Optional: true,
+								Type:     schema.TypeString,
+							},
+							"external_id": {
+								Optional: true,
+								Type:     schema.TypeString,
+							},
+							"role_session_name": {
+								Optional: true,
+								Type:     schema.TypeString,
+							},
+							"session_duration": {
+								Optional: true,
+								Type:     schema.TypeInt,
+							},
+						},
+					},
+					MaxItems: 1,
+					Optional: true,
+					Type:     schema.TypeSet,
+				},
+				"variable": {
+					Optional: true,
+					Type:     schema.TypeString,
+				},
+				"use_instance_role": {
+					Optional: true,
+					Type:     schema.TypeBool,
+				},
+			},
+		},
+		MaxItems: 1,
+		Optional: true,
+		Type:     schema.TypeSet,
+	}
+}
+
+func addTerraformTemplateAzureAccountSchema(element *schema.Resource) {
+	element.Schema["azure_account"] = &schema.Schema{
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"variable": {
+					Optional: true,
+					Type:     schema.TypeString,
+				},
+			},
+		},
+		MaxItems: 1,
+		Optional: true,
+		Type:     schema.TypeSet,
+	}
+}
+
+func addTerraformTemplateParametersSchema(element *schema.Resource) {
+	element.Schema["template_parameters"] = &schema.Schema{
+		Optional: true,
+		Type:     schema.TypeString,
+	}
+}
+
+func addTerraformTemplateSchema(element *schema.Resource) {
+	element.Schema["template"] = &schema.Schema{
+		Optional: true,
+		Type:     schema.TypeString,
+	}
+}
+
+func addTerraformTemplateSourceSchema(element *schema.Resource) {
+	element.Schema["template_source"] = &schema.Schema{
+		Default:  "Inline",
+		Optional: true,
+		Type:     schema.TypeString,
+	}
+}
+
 func expandApplyTerraformTemplateAction(flattenedAction map[string]interface{}) octopusdeploy.DeploymentAction {
 	action := expandAction(flattenedAction)
 	action.ActionType = "Octopus.TerraformApply"
 
-	if v, ok := flattenedAction["additional_init_params"]; ok {
-		action.Properties["Octopus.Action.Terraform.AdditionalInitParams"] = octopusdeploy.NewPropertyValue(v.(string), false)
+	if v, ok := flattenedAction["template"]; ok {
+		action.Properties["Octopus.Action.Terraform.Template"] = octopusdeploy.NewPropertyValue(v.(string), false)
 	}
 
-	if v, ok := flattenedAction["allow_plugin_downloads"]; ok {
-		allowPluginDownloads := v.(bool)
-		action.Properties["Octopus.Action.Terraform.AllowPluginDownloads"] = octopusdeploy.NewPropertyValue(strconv.FormatBool(allowPluginDownloads), false)
+	if v, ok := flattenedAction["template_parameters"]; ok {
+		action.Properties["Octopus.Action.Terraform.TemplateParameters"] = octopusdeploy.NewPropertyValue(v.(string), false)
 	}
 
-	if v, ok := flattenedAction["managed_account"]; ok {
-		action.Properties["Octopus.Action.Terraform.ManagedAccount"] = octopusdeploy.NewPropertyValue(v.(string), false)
+	if v, ok := flattenedAction["template_source"]; ok {
+		if v == "Inline" {
+			action.Properties["Octopus.Action.Script.ScriptSource"] = octopusdeploy.NewPropertyValue("Inline", false)
+		} else {
+			action.Properties["Octopus.Action.Script.ScriptSource"] = octopusdeploy.NewPropertyValue("Package", false)
+		}
 	}
 
-	action.Properties["Octopus.Action.Script.ScriptSource"] = octopusdeploy.NewPropertyValue("Package", false)
+	if v, ok := flattenedAction["advanced_options"]; ok && len(v.(*schema.Set).List()) > 0 {
+		advancedOptions := v.(*schema.Set).List()[0].(map[string]interface{})
+
+		if v, ok := advancedOptions["allow_additional_plugin_downloads"]; ok {
+			allowPluginDownloads := v.(bool)
+			action.Properties["Octopus.Action.Terraform.AllowPluginDownloads"] = octopusdeploy.NewPropertyValue(strconv.FormatBool(allowPluginDownloads), false)
+		}
+
+		if v, ok := advancedOptions["apply_parameters"]; ok {
+			action.Properties["Octopus.Action.Terraform.AdditionalActionParams"] = octopusdeploy.NewPropertyValue(v.(string), false)
+		}
+
+		if v, ok := advancedOptions["init_parameters"]; ok {
+			action.Properties["Octopus.Action.Terraform.AdditionalInitParams"] = octopusdeploy.NewPropertyValue(v.(string), false)
+		}
+
+		if v, ok := advancedOptions["plugin_cache_directory"]; ok {
+			action.Properties["Octopus.Action.Terraform.PluginsDirectory"] = octopusdeploy.NewPropertyValue(v.(string), false)
+		}
+
+		if v, ok := advancedOptions["workspace"]; ok {
+			action.Properties["Octopus.Action.Terraform.Workspace"] = octopusdeploy.NewPropertyValue(v.(string), false)
+		}
+	}
+
+	if v, ok := flattenedAction["aws_account"]; ok && len(v.(*schema.Set).List()) > 0 {
+		action.Properties["Octopus.Action.Terraform.ManagedAccount"] = octopusdeploy.NewPropertyValue("AWS", false)
+
+		awsAccount := v.(*schema.Set).List()[0].(map[string]interface{})
+
+		if v, ok := awsAccount["region"]; ok {
+			action.Properties["Octopus.Action.Aws.Region"] = octopusdeploy.NewPropertyValue(v.(string), false)
+		}
+
+		if v, ok := awsAccount["role"]; ok && len(v.(*schema.Set).List()) > 0 {
+			action.Properties["Octopus.Action.Aws.AssumeRole"] = octopusdeploy.NewPropertyValue("True", false)
+
+			role := v.(*schema.Set).List()[0].(map[string]interface{})
+
+			if v, ok := role["arn"]; ok {
+				action.Properties["Octopus.Action.Aws.AssumedRoleArn"] = octopusdeploy.NewPropertyValue(v.(string), false)
+			}
+
+			if v, ok := role["external_id"]; ok {
+				action.Properties["Octopus.Action.Aws.AssumeRoleExternalId"] = octopusdeploy.NewPropertyValue(v.(string), false)
+			}
+
+			if v, ok := role["role_session_name"]; ok {
+				action.Properties["Octopus.Action.Aws.AssumedRoleSession"] = octopusdeploy.NewPropertyValue(v.(string), false)
+			}
+
+			if v, ok := role["session_duration"]; ok {
+				action.Properties["Octopus.Action.Aws.AssumeRoleSessionDurationSeconds"] = octopusdeploy.NewPropertyValue(strconv.Itoa(v.(int)), false)
+			}
+		}
+
+		if v, ok := awsAccount["variable"]; ok {
+			action.Properties["Octopus.Action.AwsAccount.Variable"] = octopusdeploy.NewPropertyValue(v.(string), false)
+		}
+
+		if v, ok := awsAccount["use_instance_role"]; ok {
+			action.Properties["Octopus.Action.AwsAccount.UseInstanceRole"] = octopusdeploy.NewPropertyValue(strconv.FormatBool(v.(bool)), false)
+		}
+	}
+
+	if v, ok := flattenedAction["azure_account"]; ok && len(v.(*schema.Set).List()) > 0 {
+		action.Properties["Octopus.Action.Terraform.AzureAccount"] = octopusdeploy.NewPropertyValue("True", false)
+
+		azureAccount := v.(*schema.Set).List()[0].(map[string]interface{})
+
+		if v, ok := azureAccount["variable"]; ok {
+			action.Properties["Octopus.Action.AzureAccount.Variable"] = octopusdeploy.NewPropertyValue(v.(string), false)
+		}
+	}
 
 	return action
+}
+
+func flattenTerraformTemplateAdvancedOptions(properties map[string]octopusdeploy.PropertyValue) []interface{} {
+	if len(properties) == 0 {
+		return nil
+	}
+
+	flattenedMap := map[string]interface{}{}
+
+	for k, v := range properties {
+		switch k {
+		case "Octopus.Action.Terraform.AdditionalActionParams":
+			flattenedMap["apply_parameters"] = v.Value
+		case "Octopus.Action.Terraform.AdditionalInitParams":
+			flattenedMap["init_parameters"] = v.Value
+		case "Octopus.Action.Terraform.AllowPluginDownloads":
+			allowPluginDownloads, _ := strconv.ParseBool(v.Value)
+			flattenedMap["allow_additional_plugin_downloads"] = allowPluginDownloads
+		case "Octopus.Action.Terraform.PluginsDirectory":
+			flattenedMap["plugin_cache_directory"] = v.Value
+		case "Octopus.Action.Terraform.Workspace":
+			flattenedMap["workspace"] = v.Value
+		}
+	}
+
+	return []interface{}{flattenedMap}
+}
+
+func flattenTerraformTemplateAwsAccount(properties map[string]octopusdeploy.PropertyValue) []interface{} {
+	if len(properties) == 0 {
+		return nil
+	}
+
+	flattenedMap := map[string]interface{}{}
+
+	for k, v := range properties {
+		switch k {
+		case "Octopus.Action.Aws.AssumeRole":
+			if v.Value == "True" {
+				flattenedMap["role"] = flattenTerraformTemplateAwsRole(properties)
+			}
+		case "Octopus.Action.Aws.Region":
+			flattenedMap["region"] = v.Value
+		case "Octopus.Action.AwsAccount.Variable":
+			flattenedMap["variable"] = v.Value
+		case "Octopus.Action.AwsAccount.UseInstanceRole":
+			useInstanceRole, _ := strconv.ParseBool(v.Value)
+			flattenedMap["use_instance_role"] = useInstanceRole
+		}
+	}
+
+	return []interface{}{flattenedMap}
+}
+
+func flattenTerraformTemplateAwsRole(properties map[string]octopusdeploy.PropertyValue) []interface{} {
+	if len(properties) == 0 {
+		return nil
+	}
+
+	flattenedMap := map[string]interface{}{}
+
+	for k, v := range properties {
+		switch k {
+		case "Octopus.Action.Aws.AssumedRoleArn":
+			flattenedMap["arn"] = v.Value
+		case "Octopus.Action.Aws.AssumeRoleExternalId":
+			flattenedMap["external_id"] = v.Value
+		case "Octopus.Action.Aws.AssumedRoleSession":
+			flattenedMap["role_session_name"] = v.Value
+		case "Octopus.Action.Aws.AssumeRoleSessionDurationSeconds":
+			duration, _ := strconv.ParseInt(v.Value, 10, 32)
+			flattenedMap["session_duration"] = duration
+		}
+	}
+
+	return []interface{}{flattenedMap}
+}
+
+func flattenTerraformTemplateAzureAccount(properties map[string]octopusdeploy.PropertyValue) []interface{} {
+	if len(properties) == 0 {
+		return nil
+	}
+
+	flattenedMap := map[string]interface{}{}
+
+	if v, ok := properties["Octopus.Action.AzureAccount.Variable"]; ok {
+		flattenedMap["variable"] = v.Value
+	}
+
+	return []interface{}{flattenedMap}
 }
 
 func flattenApplyTerraformTemplateAction(action octopusdeploy.DeploymentAction) map[string]interface{} {
 	flattenedAction := flattenAction(action)
 
-	for propertyName, propertyValue := range action.Properties {
-		switch propertyName {
+	for k, v := range action.Properties {
+		switch k {
 		case "Octopus.Action.RunOnServer":
-			runOnServer, _ := strconv.ParseBool(propertyValue.Value)
+			runOnServer, _ := strconv.ParseBool(v.Value)
 			flattenedAction["run_on_server"] = runOnServer
-		case "Octopus.Action.Terraform.AdditionalInitParams":
-			flattenedAction["additional_init_params"] = propertyValue.Value
-		case "Octopus.Action.Terraform.AllowPluginDownloads":
-			allowPluginDownloads, _ := strconv.ParseBool(propertyValue.Value)
-			flattenedAction["allow_plugin_downloads"] = allowPluginDownloads
+		case "Octopus.Action.Script.ScriptSource":
+			flattenedAction["template_source"] = v.Value
+		case "Octopus.Action.Terraform.AzureAccount":
+			if v.Value == "True" {
+				flattenedAction["azure_account"] = flattenTerraformTemplateAzureAccount(action.Properties)
+			}
 		case "Octopus.Action.Terraform.ManagedAccount":
-			flattenedAction["managed_account"] = propertyValue.Value
+			if v.Value == "AWS" {
+				flattenedAction["aws_account"] = flattenTerraformTemplateAwsAccount(action.Properties)
+			}
+		case "Octopus.Action.Terraform.Template":
+			flattenedAction["template"] = v.Value
+		case "Octopus.Action.Terraform.TemplateParameters":
+			flattenedAction["template_parameters"] = v.Value
 		}
 	}
+
+	flattenedAction["advanced_options"] = flattenTerraformTemplateAdvancedOptions(action.Properties)
 
 	return flattenedAction
 }
@@ -53,25 +349,13 @@ func flattenApplyTerraformTemplateAction(action octopusdeploy.DeploymentAction) 
 func getApplyTerraformTemplateActionSchema() *schema.Schema {
 	actionSchema, element := getActionSchema()
 	addExecutionLocationSchema(element)
+	addTerraformTemplateAdvancedOptionsSchema(element)
+	addTerraformTemplateAwsAccountSchema(element)
+	addTerraformTemplateAzureAccountSchema(element)
+	addTerraformTemplateParametersSchema(element)
+	addTerraformTemplateSchema(element)
+	addTerraformTemplateSourceSchema(element)
 	addPrimaryPackageSchema(element, false)
-
-	element.Schema["additional_init_params"] = &schema.Schema{
-		Type:        schema.TypeString,
-		Description: "Additional parameters passed to the init command",
-		Optional:    true,
-	}
-
-	element.Schema["allow_plugin_downloads"] = &schema.Schema{
-		Computed: true,
-		Type:     schema.TypeBool,
-		Optional: true,
-	}
-
-	element.Schema["managed_account"] = &schema.Schema{
-		Computed: true,
-		Type:     schema.TypeString,
-		Optional: true,
-	}
 
 	return actionSchema
 }
