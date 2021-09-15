d60a6bc19f43571dec3f85b2148836e36db4789f
Matt Whitney <matt.whitney@formidable.com>
2021-09-15 08:44:41 -0400

    fix: Update octopusdeploy_deployment_process example to use run_script_action.
    Fixes #246

--- examples/resources/octopusdeploy_deployment_process/resource.tf
+++ examples/resources/octopusdeploy_deployment_process/resource.tf
@@ -5,24 +5,18 @@ resource "octopusdeploy_deployment_process" "example" {
     name                = "Hello world (using PowerShell)"
     package_requirement = "LetOctopusDecide"
     start_trigger       = "StartAfterPrevious"
-    action {
-      action_type                        = "Octopus.Script"
+    run_script_action {
       can_be_used_for_project_versioning = false
       condition                          = "Success"
       is_disabled                        = false
       is_required                        = true
       name                               = "Hello world (using PowerShell)"
-      properties = {
-        "Octopus.Action.RunOnServer"         = "true"
-        "Octopus.Action.Script.ScriptBody"   = <<-EOT
-                    Write-Host 'Hello world, using PowerShell'
-                    #TODO: Experiment with steps of your own :)
-                    Write-Host '[Learn more about the types of steps available in Octopus](https://g.octopushq.com/OnboardingAddStepsLearnMore)'
-                EOT
-        "Octopus.Action.Script.ScriptSource" = "Inline"
-        "Octopus.Action.Script.Syntax"       = "PowerShell"
-      }
-      run_on_server = false
+      script_body                        = <<-EOT
+          Write-Host 'Hello world, using PowerShell'
+          #TODO: Experiment with steps of your own :)
+          Write-Host '[Learn more about the types of steps available in Octopus](https://g.octopushq.com/OnboardingAddStepsLearnMore)'
+        EOT
+      run_on_server                      = true
     }
   }
   step {
@@ -30,24 +24,18 @@ resource "octopusdeploy_deployment_process" "example" {
     name                = "Hello world (using Bash)"
     package_requirement = "LetOctopusDecide"
     start_trigger       = "StartWithPrevious"
-    action {
-      action_type                        = "Octopus.Script"
+    run_script_action {
       can_be_used_for_project_versioning = false
       condition                          = "Success"
       is_disabled                        = false
       is_required                        = true
       name                               = "Hello world (using Bash)"
-      properties = {
-        "Octopus.Action.RunOnServer"         = "true"
-        "Octopus.Action.Script.ScriptBody"   = <<-EOT
-                    echo 'Hello world, using Bash'
-                    #TODO: Experiment with steps of your own :)
-                    echo '[Learn more about the types of steps available in Octopus](https://g.octopushq.com/OnboardingAddStepsLearnMore)'
-                EOT
-        "Octopus.Action.Script.ScriptSource" = "Inline"
-        "Octopus.Action.Script.Syntax"       = "Bash"
-      }
-      run_on_server = false
+      script_body                        = <<-EOT
+          echo 'Hello world, using Bash'
+          #TODO: Experiment with steps of your own :)
+          echo '[Learn more about the types of steps available in Octopus](https://g.octopushq.com/OnboardingAddStepsLearnMore)'
+        EOT
+      run_on_server                      = true
     }
   }
 }
