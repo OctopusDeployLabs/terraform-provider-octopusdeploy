47358fd05b71a5ce1a857a139d4ccbcc22658704
slewis74 <slewis74@gmail.com>
2022-01-12 20:04:32 +1000

    Config was using Name search for Spaces, which Octopus doesn't actually support. Switched to PartialName search, added error checking on returned results, and added some DEBUG logging to help anyone trying to troubleshoot config setup issues.

--- octopusdeploy/config.go
+++ octopusdeploy/config.go
@@ -1,7 +1,10 @@
 package octopusdeploy
 
 import (
+	"fmt"
+	"log"
 	"net/url"
+	"strings"
 
 	"github.com/OctopusDeploy/go-octopusdeploy/octopusdeploy"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -28,6 +31,7 @@ func (c *Config) Client() (*octopusdeploy.Client, diag.Diagnostics) {
 	}
 
 	if len(c.SpaceID) > 0 {
+		log.Printf("[DEBUG] Config is locating space using ID '%s'", c.SpaceID)
 		space, err := client.Spaces.GetByID(c.SpaceID)
 		if err != nil {
 			return nil, diag.FromErr(err)
@@ -40,18 +44,37 @@ func (c *Config) Client() (*octopusdeploy.Client, diag.Diagnostics) {
 	}
 
 	if len(c.SpaceName) > 0 {
+		log.Printf("[DEBUG] Config is locating space using name '%s'", c.SpaceName)
 		spaces, err := client.Spaces.Get(octopusdeploy.SpacesQuery{
-			Name: c.SpaceName,
+			PartialName: c.SpaceName,
 		})
 		if err != nil {
 			return nil, diag.FromErr(err)
 		}
 
-		client, err = octopusdeploy.NewClient(nil, apiURL, c.APIKey, spaces.Items[0].GetID())
+		if spaces.TotalResults == 0 {
+			return nil, diag.Errorf("Unable to locate space with name '%s', found no spaces", c.SpaceName)
+		}
+		if spaces.TotalResults > 1 {
+			return nil, diag.Errorf("Unable to uniquely locate space with name '%s', found spaces %s", c.SpaceName, strings.Join(getQuotedSpaceNames(spaces.Items), ", "))
+		}
+
+		spaceID := spaces.Items[0].GetID()
+		log.Printf("[DEBUG] Config located space using name '%s', which has ID '%s'", c.SpaceName, spaceID)
+
+		client, err = octopusdeploy.NewClient(nil, apiURL, c.APIKey, spaceID)
 		if err != nil {
 			return nil, diag.FromErr(err)
 		}
 	}
 
 	return client, nil
+}
+
+func getQuotedSpaceNames(spaces []octopusdeploy.Space) []string {
+	var newSlice []string
+	for _, space := range spaces {
+		newSlice = append(newSlice, fmt.Sprintf("'%s'", space.Name))
+	}
+	return newSlice
 }
