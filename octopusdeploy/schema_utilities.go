1a2356fffaa66e00cf2f3033a80193324f8fcd49
John Simons <john.simons@octopus.com>
2022-02-01 15:35:30 +1000

    Updating tests

--- octopusdeploy/schema_utilities.go
+++ octopusdeploy/schema_utilities.go
@@ -404,7 +404,7 @@ func getUsernameSchema(isRequired bool) *schema.Schema {
 
 func getVariableTypeSchema() *schema.Schema {
 	return &schema.Schema{
-		Description: "The type of variable represented by this resource. Valid types are `AmazonWebServicesAccount`, `AzureAccount`, `Certificate`, `Sensitive`, `String`, or `WorkerPool`.",
+		Description: "The type of variable represented by this resource. Valid types are `AmazonWebServicesAccount`, `AzureAccount`, `GoogleCloudAccount`, `Certificate`, `Sensitive`, `String`, or `WorkerPool`.",
 		Required:    true,
 		Type:        schema.TypeString,
 		ValidateDiagFunc: validation.ToDiagFunc(validation.StringInSlice([]string{
