ae00313dfcce2d45437a871c9c45361604cbff91
John Bristowe <john.bristowe@octopus.com>
2022-10-19 08:55:38 +1000

    chore: updated documentation

--- examples/data-sources/octopusdeploy_cloud_region_deployment_targets/data-source.tf
+++ examples/data-sources/octopusdeploy_cloud_region_deployment_targets/data-source.tf
@@ -1,7 +1,7 @@
 data "octopusdeploy_cloud_region_deployment_targets" "example" {
   deployment_id = "Defau"
   environments  = ["Environments-123", "Environments-321"]
-  ids = 
+  ids           = ["Machines-123"]
   skip          = 5
   take          = 100
 }
