d9e6bb4326b2dae77910e7f58ba2f1da854babf0
Domenic Simone <domenic.simone@octopus.com>
2023-10-17 09:36:47 +1100

    feat: spaceID azure sp account

--- octopusdeploy/resource_azure_service_principal_account.go
+++ octopusdeploy/resource_azure_service_principal_account.go
@@ -29,7 +29,7 @@ func resourceAzureServicePrincipalAccountCreate(ctx context.Context, d *schema.R
 	log.Printf("[INFO] creating Azure service principal account: %#v", account)
 
 	client := m.(*client.Client)
-	createdAccount, err := client.Accounts.Add(account)
+	createdAccount, err := accounts.Add(client, account)
 	if err != nil {
 		return diag.FromErr(err)
 	}
@@ -48,7 +48,7 @@ func resourceAzureServicePrincipalAccountDelete(ctx context.Context, d *schema.R
 	log.Printf("[INFO] deleting Azure service principal account (%s)", d.Id())
 
 	client := m.(*client.Client)
-	if err := client.Accounts.DeleteByID(d.Id()); err != nil {
+	if err := accounts.DeleteByID(client, d.Get("space_id").(string), d.Id()); err != nil {
 		return diag.FromErr(err)
 	}
 
@@ -62,7 +62,7 @@ func resourceAzureServicePrincipalAccountRead(ctx context.Context, d *schema.Res
 	log.Printf("[INFO] reading Azure service principal account (%s)", d.Id())
 
 	client := m.(*client.Client)
-	accountResource, err := client.Accounts.GetByID(d.Id())
+	accountResource, err := accounts.GetByID(client, d.Get("space_id").(string), d.Id())
 	if err != nil {
 		return errors.ProcessApiError(ctx, d, err, "Azure service principal account")
 	}
@@ -82,7 +82,7 @@ func resourceAzureServicePrincipalAccountUpdate(ctx context.Context, d *schema.R
 	log.Printf("[INFO] updating Azure service principal account %#v", account)
 
 	client := m.(*client.Client)
-	updatedAccount, err := client.Accounts.Update(account)
+	updatedAccount, err := accounts.Update(client, account)
 	if err != nil {
 		return diag.FromErr(err)
 	}
