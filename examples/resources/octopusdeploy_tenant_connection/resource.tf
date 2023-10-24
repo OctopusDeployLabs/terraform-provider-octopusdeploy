1f589fe8f11e75824d63b6e86fd17db5d4485960
Hans Song <hans.m.song@gmail.com>
2023-10-24 21:55:43 +1000

    update documentation

--- examples/resources/octopusdeploy_tenant_connection/resource.tf
+++ examples/resources/octopusdeploy_tenant_connection/resource.tf
@@ -1,5 +1,13 @@
+resource "octopusdeploy_tenant" "example" {
+  name = "Example Tenant"
+
+  lifecycle {
+    ignore_changes = [project_environment]
+  }
+}
+
 resource "octopusdeploy_tenant_connection" "example" {
-  tenant_id       = "Tenants-123"
+  tenant_id       = octopusdeploy_tenant.example.id
   project_id      = "Projects-123"
   environment_ids = ["Environments-1", "Environments-2", "Environments-3"]
 }
