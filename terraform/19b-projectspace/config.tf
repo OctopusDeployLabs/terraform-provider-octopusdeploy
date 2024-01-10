9de544dba265d17d74596381e35628f8e1ad0821
Travis Leeden <travis.leeden@octopus.com>
2024-01-10 15:29:37 +1000

    Test if test config needed updated provider version

--- terraform/19b-projectspace/config.tf
+++ terraform/19b-projectspace/config.tf
@@ -1,6 +1,6 @@
 terraform {
   required_providers {
-    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.11.3" }
+    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.14.3" }
     // Use the option below when debugging
     // octopusdeploy = { source = "octopus.com/com/octopusdeploy" }
   }
