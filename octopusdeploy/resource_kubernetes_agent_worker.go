e44242fdc44eb950727dc59fba84bdd95e2f341e
Trent Mohay <trent.mohay@octopus.com>
2024-06-17 16:33:53 +1000

    add k8s worker to terraform

---
+++ octopusdeploy/resource_kubernetes_agent_worker.go
@@ -0,0 +1,78 @@
+package octopusdeploy
+
+import (
+	"context"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/client"
+	"github.com/OctopusDeploy/terraform-provider-octopusdeploy/internal/errors"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceKubernetesAgentWorker() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceKubernetesAgentWorkerCreate,
+		DeleteContext: resourceKubernetesAgentWorkerDelete,
+		Description:   "This resource manages Kubernetes agent deployment targets in Octopus Deploy.",
+		Importer:      getImporter(),
+		ReadContext:   resourceKubernetesAgentWorkerRead,
+		Schema:        getKubernetesAgentWorkerSchema(),
+		UpdateContext: resourceKubernetesAgentWorkerUpdate,
+	}
+}
+
+func resourceKubernetesAgentWorkerCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	worker := expandKubernetesAgentWorker(d)
+	client := m.(*client.Client)
+	createdWorker, err := client.Workers.Add(worker)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(createdWorker.GetID())
+	return nil
+}
+
+func resourceKubernetesAgentWorkerRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*client.Client)
+	Worker, err := client.Workers.GetByID(d.Id())
+	if err != nil {
+		return errors.ProcessApiError(ctx, d, err, "kubernetes tentacle deployment target")
+	}
+
+	flattenedKubernetesAgentWorker := flattenKubernetesAgentWorker(Worker)
+	for key, value := range flattenedKubernetesAgentWorker {
+		if key != "id" {
+			err := d.Set(key, value)
+			if err != nil {
+				return diag.FromErr(err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func resourceKubernetesAgentWorkerDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*client.Client)
+	if err := client.Workers.DeleteByID(d.Id()); err != nil {
+		return diag.FromErr(err)
+	}
+	d.SetId("")
+	return nil
+}
+
+func resourceKubernetesAgentWorkerUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	Worker := expandKubernetesAgentWorker(d)
+	client := m.(*client.Client)
+
+	Worker.ID = d.Id()
+
+	updatedWorker, err := client.Workers.Update(Worker)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(updatedWorker.GetID())
+
+	return nil
+}
