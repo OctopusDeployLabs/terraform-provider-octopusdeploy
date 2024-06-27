72fd4c78888cfe29896e7c63d3919a1f3b8bcede
Trent Mohay <trent.mohay@octopus.com>
2024-06-27 13:28:48 +1000

    move files around for testing

---
+++ terraform/58-kubernetesagentworker/kubernetes_agent_workers.tf
@@ -0,0 +1,18 @@
+resource "octopusdeploy_kubernetes_agent_worker" "agent_with_minimum" {
+  name         = "minimum-agent"
+  worker_pool_ids = [octopusdeploy_static_worker_pool.workerpool_docker.id]
+  uri          = "poll://kcxzcv2fpsxkn6tk9u6d/"
+  thumbprint   = "96203ED84246201C26A2F4360D7CBC36AC1D232D"
+}
+
+resource "octopusdeploy_kubernetes_agent_worker" "agent_with_optional" {
+  name               = "optional-agent"
+  machine_policy_id  = octopusdeploy_machine_policy.machinepolicy_testing.id
+  worker_pool_ids = [octopusdeploy_static_worker_pool.workerpool_docker.id]
+  communication_mode = "Polling"
+  uri                = "poll://kcxzcv2fpsxkn6tk9u6d/"
+  thumbprint         = "96203ED84246201C26A2F4360D7CBC36AC1D232D"
+  default_namespace  = "kubernetes-namespace"
+  is_disabled        = true
+  upgrade_locked     = true
+}
