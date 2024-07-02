a968af9a792f926e378e7d39967760e835d115d2
Ben Pearce <ben.pearce@octopus.com>
2024-07-02 17:27:01 +1000

    introduced new protocol v6 server and muxing server to old provider

---
+++ octopusdeploy/providerv6.go
@@ -0,0 +1,63 @@
+package octopusdeploy
+
+import (
+	"context"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+)
+
+type octopusDeployProviderV6 struct {
+	Address types.String `tfsdk:"address"`
+	ApiKey  types.String `tfsdk:"api_key"`
+	SpaceID types.String `tfsdk:"space_id"`
+}
+
+var _ provider.Provider = (*octopusDeployProviderV6)(nil)
+var _ provider.ProviderWithMetaSchema = (*octopusDeployProviderV6)(nil)
+
+func NewOctopusDeployProviderV6() *octopusDeployProviderV6 {
+	return &octopusDeployProviderV6{}
+}
+
+func (p *octopusDeployProviderV6) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+}
+
+func (p *octopusDeployProviderV6) MetaSchema(ctx context.Context, request provider.MetaSchemaRequest, response *provider.MetaSchemaResponse) {
+
+}
+
+func (p *octopusDeployProviderV6) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+}
+
+func (p *octopusDeployProviderV6) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{}
+}
+
+func (p *octopusDeployProviderV6) Resources(ctx context.Context) []func() resource.Resource {
+	return []func() resource.Resource{}
+}
+
+func (p *octopusDeployProviderV6) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"address": schema.StringAttribute{
+				Optional:    false,
+				Required:    true,
+				Description: "The endpoint of the Octopus REST API",
+			},
+			"api_key": schema.StringAttribute{
+				Optional:    false,
+				Required:    true,
+				Description: "The API key to use with the Octopus REST API",
+			},
+			"space_id": schema.StringAttribute{
+				Optional:    true,
+				Description: "The space ID to target",
+			},
+		},
+	}
+}
