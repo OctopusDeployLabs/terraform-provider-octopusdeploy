3ad4547c3fbba2ab8a0b6b17bfaecc5ed37f82f9
isaac <isaac.calligeros@octopus.com>
2024-07-07 17:43:38 +0930

    Comment out terratest _test files. Unsure at this stage if these will be re-instated during migrations but these are currently out-dated

--- terratest/deploymenttrigger_creation_test.go
+++ terratest/deploymenttrigger_creation_test.go
@@ -1,28 +1,28 @@
-package terratest
-
-import (
-	"fmt"
-	"testing"
-
-	"github.com/gruntwork-io/terratest/modules/terraform"
-)
-
-func TestDeploymentTriggerCreation(test *testing.T) {
-	terraformTest := &terraform.Options{
-		TerraformDir: "../examples/Deployment-Trigger-Creation",
-	}
-
-	defer terraform.Destroy(test, terraformTest)
-
-	if _, err := terraform.InitE(test, terraformTest); err != nil {
-		fmt.Println(err)
-	}
-
-	if _, err := terraform.PlanE(test, terraformTest); err != nil {
-		fmt.Println(err)
-	}
-
-	if _, err := terraform.ApplyE(test, terraformTest); err != nil {
-		fmt.Println(err)
-	}
-}
+//package terratest
+//
+//import (
+//	"fmt"
+//	"testing"
+//
+//	"github.com/gruntwork-io/terratest/modules/terraform"
+//)
+//
+//func TestDeploymentTriggerCreation(test *testing.T) {
+//	terraformTest := &terraform.Options{
+//		TerraformDir: "../examples/Deployment-Trigger-Creation",
+//	}
+//
+//	defer terraform.Destroy(test, terraformTest)
+//
+//	if _, err := terraform.InitE(test, terraformTest); err != nil {
+//		fmt.Println(err)
+//	}
+//
+//	if _, err := terraform.PlanE(test, terraformTest); err != nil {
+//		fmt.Println(err)
+//	}
+//
+//	if _, err := terraform.ApplyE(test, terraformTest); err != nil {
+//		fmt.Println(err)
+//	}
+//}
