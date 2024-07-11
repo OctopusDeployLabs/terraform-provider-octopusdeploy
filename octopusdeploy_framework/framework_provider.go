40011fd1f5c26028ee108feb4ed78691ce3f44b2
Ben Pearce <ben.pearce@octopus.com>
2024-07-11 11:28:45 +1000

    register project group resources

--- octopusdeploy_framework/framework_provider.go
+++ octopusdeploy_framework/framework_provider.go
@@ -54,11 +54,14 @@ func (p *octopusDeployFrameworkProvider) DataSources(ctx context.Context) []func
 	return []func() datasource.DataSource{
 		NewSpaceDataSource,
 		NewSpacesDataSource,
+		NewProjectGroupsDataSource,
 	}
 }
 
 func (p *octopusDeployFrameworkProvider) Resources(ctx context.Context) []func() resource.Resource {
-	return []func() resource.Resource{}
+	return []func() resource.Resource{
+		NewProjectGroupResource,
+	}
 }
 
 func (p *octopusDeployFrameworkProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
