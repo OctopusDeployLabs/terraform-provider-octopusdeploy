235ddc856360ea312e552bf481d919547238c3bd
Robert E <robert.erez@gmail.com>
2024-07-15 21:55:45 +1000

    more tests

--- terraform/24-tenants/tenant.tf
+++ terraform/24-tenants/tenant.tf
@@ -3,9 +3,23 @@ resource "octopusdeploy_tenant" "tenant_team_a" {
   description = "Test tenant"
   tenant_tags = ["tag1/a", "tag1/b"]
   depends_on = [octopusdeploy_tag.tag_a, octopusdeploy_tag.tag_b]
+}
 
-  project_environment {
-    environments = [octopusdeploy_environment.test_environment.id, octopusdeploy_environment.development_environment.id, octopusdeploy_environment.production_environment.id]
-    project_id   = octopusdeploy_project.deploy_frontend_project.id
-  }
+
+resource "octopusdeploy_tenant_project_environment" "team_a_test_environment" {
+  tenant_id = octopusdeploy_tenant.tenant_team_a.id
+  project_id = octopusdeploy_project.deploy_frontend_project.id
+  environment_id = octopusdeploy_environment.test_environment.id
+}
+
+resource "octopusdeploy_tenant_project_environment" "team_a_development_environment" {
+  tenant_id = octopusdeploy_tenant.tenant_team_a.id
+  project_id = octopusdeploy_project.deploy_frontend_project.id
+  environment_id = octopusdeploy_environment.development_environment.id
+}
+
+resource "octopusdeploy_tenant_project_environment" "team_a_production_environment" {
+  tenant_id = octopusdeploy_tenant.tenant_team_a.id
+  project_id = octopusdeploy_project.deploy_frontend_project.id
+  environment_id = octopusdeploy_environment.production_environment.id
 }
