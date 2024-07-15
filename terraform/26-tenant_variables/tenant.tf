9b9a5dd340b25e8ccdf5edb46f9455fe22508645
Robert E <robert.erez@gmail.com>
2024-07-15 21:48:57 +1000

    More tests

--- terraform/26-tenant_variables/tenant.tf
+++ terraform/26-tenant_variables/tenant.tf
@@ -1,9 +1,22 @@
 resource "octopusdeploy_tenant" "tenant_team_a" {
   name        = "Team A"
   description = "Test tenant"
+}
 
-  project_environment {
-    environments = [octopusdeploy_environment.test_environment.id, octopusdeploy_environment.development_environment.id, octopusdeploy_environment.production_environment.id]
-    project_id   = octopusdeploy_project.deploy_frontend_project.id
-  }
+resource "octopusdeploy_tenant_project_environment" "team_a_test_environment" {
+  tenant_id = octopusdeploy_tenant.tenant_team_a.id
+  project_id = octopusdeploy_project.deploy_frontend_project.id
+  environment_id = octopusdeploy_environment.test_environment.id
+}
+
+resource "octopusdeploy_tenant_project_environment" "team_a_development_environment" {
+  tenant_id = octopusdeploy_tenant.tenant_team_a.id
+  project_id = octopusdeploy_project.deploy_frontend_project.id
+  environment_id = octopusdeploy_environment.development_environment.id
+}
+
+resource "octopusdeploy_tenant_project_environment" "team_a_production_environment" {
+  tenant_id = octopusdeploy_tenant.tenant_team_a.id
+  project_id = octopusdeploy_project.deploy_frontend_project.id
+  environment_id = octopusdeploy_environment.production_environment.id
 }
