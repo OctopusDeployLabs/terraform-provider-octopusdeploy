249cdee9a27c2f4cbd9221d921e3dad03f115b0d
hnrkndrssn <henrik@henrikandersson.org>
2024-07-23 12:57:05 +1000

    chore: move some helper functions around

--- octopusdeploy_framework/util/util.go
+++ octopusdeploy_framework/util/util.go
@@ -3,6 +3,7 @@ package util
 import (
 	"context"
 	"fmt"
+
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -78,10 +79,32 @@ func GetStringSlice(list types.List) []string {
 	return result
 }
 
+func StringArrayToListOrNullIfEmpty(itemType attr.Type, items []string) types.List {
+	if len(items) > 0 {
+		return types.ListValueMust(itemType, ToValueSlice(items))
+	}
+
+	return types.ListNull(itemType)
+}
+
 func ToValueSlice(slice []string) []attr.Value {
 	values := make([]attr.Value, len(slice))
 	for i, s := range slice {
 		values[i] = types.StringValue(s)
 	}
 	return values
+}
+
+func GetStringSlice(list types.List) []string {
+	if list.IsNull() || list.IsUnknown() {
+		return nil
+	}
+
+	result := make([]string, 0, len(list.Elements()))
+	for _, element := range list.Elements() {
+		if str, ok := element.(types.String); ok {
+			result = append(result, str.ValueString())
+		}
+	}
+	return result
 }
