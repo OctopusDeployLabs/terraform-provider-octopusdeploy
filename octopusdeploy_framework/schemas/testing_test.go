12c4b1a0a4ca3b9e8e0018611d2de4d6147b2b1f
isaac <isaac.calligeros@octopus.com>
2024-07-26 15:50:06 +0930

    With flag

--- octopusdeploy_framework/schemas/testing_test.go
+++ octopusdeploy_framework/schemas/testing_test.go
@@ -1,10 +1,13 @@
 package schemas
 
 import (
+	"flag"
 	"github.com/stretchr/testify/assert"
 	"log"
 	"testing"
 )
+
+var createSharedContainer = flag.Bool("createSharedContainer", false, "Set to true to run integration tests in containers")
 
 func TestSomething(t *testing.T) {
 	log.Print("Hi")
