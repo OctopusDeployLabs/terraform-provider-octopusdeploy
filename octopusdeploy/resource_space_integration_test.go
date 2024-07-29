c89fb833ca619045a8f0db3b9e0969dcea6dcf45
isaac <isaac.calligeros@octopus.com>
2024-07-29 15:59:14 +0930

    Fix spaces test

--- octopusdeploy/resource_space_integration_test.go
+++ octopusdeploy/resource_space_integration_test.go
@@ -31,8 +31,8 @@ func (suite *IntegrationTestSuite) TestSpaceResource() {
 		t.Fatal(err.Error())
 	}
 
-	if space.Description != "The description of the new space" {
-		t.Fatalf("New space must have the description \"The description of the new space\"")
+	if space.Description != "TestSdkIntegrationTestSuite/TestSpaceResource" {
+		t.Fatalf("New space must have the description \"TestSdkIntegrationTestSuite/TestSpaceResource")
 	}
 
 	if space.IsDefault {
