efe31e4e5de66395819fa6841cc5dbcf68080d50
hnrkndrssn <henrik@henrikandersson.org>
2024-07-30 19:06:46 +1000

    chore: mutexes are :fire:

--- octopusdeploy_framework/framework_provider.go
+++ octopusdeploy_framework/framework_provider.go
@@ -3,8 +3,8 @@ package octopusdeploy_framework
 import (
 	"context"
 	"os"
+	"sync"
 
-	"github.com/OctopusDeploy/terraform-provider-octopusdeploy/internal"
 	"github.com/OctopusDeploy/terraform-provider-octopusdeploy/octopusdeploy_framework/util"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
@@ -13,7 +13,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-var mutex = internal.Mutex
+var mutex = &sync.Mutex{}
 
 type octopusDeployFrameworkProvider struct {
 	Address types.String `tfsdk:"address"`
