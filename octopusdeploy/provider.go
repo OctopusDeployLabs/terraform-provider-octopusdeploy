ae275e159a3a51be76503646d2d3e708370d9eb6
hnrkndrssn <henrik@henrikandersson.org>
2024-07-30 19:06:46 +1000

    chore: mutexes are :fire:

--- octopusdeploy/provider.go
+++ octopusdeploy/provider.go
@@ -2,13 +2,13 @@ package octopusdeploy
 
 import (
 	"context"
+	"sync"
 
-	"github.com/OctopusDeploy/terraform-provider-octopusdeploy/internal"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-var mutex = internal.Mutex
+var mutex = &sync.Mutex{}
 
 // Provider is the plugin entry point for the Terraform provider for Octopus Deploy.
 func Provider() *schema.Provider {
