745fb275be7e9bd711ebea271ce91acf1212e277
Ben Pearce <ben.pearce@octopus.com>
2024-07-31 11:26:10 +1000

    extended test

--- octopusdeploy_framework/resource_tenant_project_migration_test.go
+++ octopusdeploy_framework/resource_tenant_project_migration_test.go
@@ -39,6 +39,13 @@ func TestTenantProjectResource_UpgradeFromSDK_ToPluginFramework(t *testing.T) {
 			},
 			{
 				ProtoV6ProviderFactories: ProtoV6ProviderFactories(),
+				Config:                   updatedTenantConfig(name),
+				Check: resource.ComposeTestCheckFunc(
+					testTenantUpdated(t, name),
+				),
+			},
+			{
+				ProtoV6ProviderFactories: ProtoV6ProviderFactories(),
 				Config:                   updatedTenantProjectConfig(name),
 				Check: resource.ComposeTestCheckFunc(
 					testTenantProjectUpdated(t),
@@ -52,6 +59,34 @@ func tenantProjectConfig(name string) string {
 	return fmt.Sprintf(`
 	resource "octopusdeploy_tenant" "tenant1" {
 		name        = "tenant %[1]s"
+	}
+
+	resource "octopusdeploy_project" "project1" {
+		name = "project %[1]s"	
+		lifecycle_id = "Lifecycles-1"
+		project_group_id = "ProjectGroups-1"
+	}
+
+	resource "octopusdeploy_environment" "environment1" {
+		name = "environment %[1]s"
+	}
+
+	resource "octopusdeploy_environment" "environment2" {
+		name = "environment %[1]s 2" 
+	}
+
+	resource "octopusdeploy_tenant_project" "project_environment" {
+		tenant_id = octopusdeploy_tenant.tenant1.id
+		project_id   = octopusdeploy_project.project1.id
+		environment_ids = [octopusdeploy_environment.environment1.id, octopusdeploy_environment.environment2.id]
+    }`, name)
+}
+
+func updatedTenantConfig(name string) string {
+	return fmt.Sprintf(`
+	resource "octopusdeploy_tenant" "tenant1" {
+		name        = "tenant %[1]s"
+		description = "description %[1]s"
 	}
 
 	resource "octopusdeploy_project" "project1" {
@@ -113,6 +148,27 @@ func testTenantProjectDestroyed(s *terraform.State) error {
 	}
 
 	return nil
+}
+
+func testTenantUpdated(t *testing.T, name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		tenantId := s.RootModule().Resources["octopusdeploy_tenant.tenant1"].Primary.ID
+		projectId := s.RootModule().Resources["octopusdeploy_project.project1"].Primary.ID
+		environmentId := s.RootModule().Resources["octopusdeploy_environment.environment1"].Primary.ID
+		environment2Id := s.RootModule().Resources["octopusdeploy_environment.environment2"].Primary.ID
+		tenant, err := octoClient.Tenants.GetByID(tenantId)
+		if err != nil {
+			return fmt.Errorf("failed to retrieve tenant by ID: %s", err)
+		}
+
+		assert.NotEmpty(t, tenant.ID, "Tenant ID did not match expected value")
+		assert.Equal(t, tenant.Description, fmt.Sprintf("description %s", name))
+		assert.Equal(t, len(tenant.ProjectEnvironments[projectId]), 2, "environments collection should only have 1 entry")
+		assert.Equal(t, tenant.ProjectEnvironments[projectId][0], environmentId, "environments collection should contain id for environment1")
+		assert.Equal(t, tenant.ProjectEnvironments[projectId][1], environment2Id, "environments collection should contain id for environment2")
+
+		return nil
+	}
 }
 
 func testTenantProjectUpdated(t *testing.T) resource.TestCheckFunc {
