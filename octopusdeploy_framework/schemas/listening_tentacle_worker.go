32c8e91f58789bde4ba54166acc60cc9efb250e3
denys.kostynchuk <denys.kostynchuk@octopus.com>
2024-11-14 11:11:32 +1300

    Use Set type instead of List type to avoid issues with inconsistent ordering

--- octopusdeploy_framework/schemas/listening_tentacle_worker.go
+++ octopusdeploy_framework/schemas/listening_tentacle_worker.go
@@ -1,7 +1,7 @@
 package schemas
 
 import (
-	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/setvalidator"
 	datasourceSchema "github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	resourceSchema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
@@ -22,12 +22,12 @@ func (m ListeningTentacleWorkerSchema) GetResourceSchema() resourceSchema.Schema
 			"uri":               GetRequiredStringResourceSchema("The network address at which the Tentacle can be reached"),
 			"thumbprint":        GetRequiredStringResourceSchema("The X509 certificate thumbprint that securely identifies the Tentacle"),
 			"proxy_id":          GetOptionalStringResourceSchema("Specify the connection type for the Tentacle: direct(when not set) or via a proxy server."),
-			"worker_pool_ids": resourceSchema.ListAttribute{
+			"worker_pool_ids": resourceSchema.SetAttribute{
 				ElementType: types.StringType,
 				Description: "Select at least one worker pool for the worker",
 				Required:    true,
-				Validators: []validator.List{
-					listvalidator.SizeAtLeast(1),
+				Validators: []validator.Set{
+					setvalidator.SizeAtLeast(1),
 				},
 			},
 		},
@@ -44,7 +44,7 @@ type ListeningTentacleWorkerResourceModel struct {
 	Name            types.String `tfsdk:"name"`
 	SpaceID         types.String `tfsdk:"space_id"`
 	IsDisabled      types.Bool   `tfsdk:"is_disabled"`
-	WorkerPoolIDs   types.List   `tfsdk:"worker_pool_ids"`
+	WorkerPoolIDs   types.Set    `tfsdk:"worker_pool_ids"`
 	MachinePolicyID types.String `tfsdk:"machine_policy_id"`
 	Uri             types.String `tfsdk:"uri"`
 	Thumbprint      types.String `tfsdk:"thumbprint"`
