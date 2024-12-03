e7d28788cee744dcdbf2273c0dd51b17476b08de
Grace Rehn <grace.rehn@octopus.com>
2024-12-03 15:43:24 +1000

    fix: docs again

--- examples/resources/octopusdeploy_generic_oidc_account/resource.tf
+++ examples/resources/octopusdeploy_generic_oidc_account/resource.tf
@@ -1,4 +1,4 @@
-resource "octopusdeploy_generic_oidc_connect" "example" {
+resource "octopusdeploy_generic_oidc_account" "example" {
   name            = "Generic OpenID Connect Account (OK to Delete)"
   execution_subject_keys = ["space", "project"]
   audience = "api://default"
