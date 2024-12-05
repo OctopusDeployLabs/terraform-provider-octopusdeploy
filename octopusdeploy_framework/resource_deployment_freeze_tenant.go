74c8b9d9ac3a591cc2e0022ed2674ce94c31796e
Huy Nguyen <huy.nguyen@octopus.com>
2024-12-05 15:00:27 +1000

    Add tenant scope resource

---
+++ octopusdeploy_framework/resource_deployment_freeze_tenant.go
@@ -0,0 +1,224 @@
+package octopusdeploy_framework
+
+import (
+	"context"
+	"fmt"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/core"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/deploymentfreezes"
+	"github.com/OctopusDeploy/terraform-provider-octopusdeploy/internal"
+	"github.com/OctopusDeploy/terraform-provider-octopusdeploy/octopusdeploy_framework/schemas"
+	"github.com/OctopusDeploy/terraform-provider-octopusdeploy/octopusdeploy_framework/util"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"net/http"
+)
+
+type deploymentFreezeTenantResource struct {
+	*Config
+}
+
+const tenantDescription = "deployment freeze tenant scope"
+
+var _ resource.Resource = &deploymentFreezeTenantResource{}
+var _ resource.ResourceWithConfigure = &deploymentFreezeTenantResource{}
+
+func NewDeploymentFreezeTenantResource() resource.Resource {
+	return &deploymentFreezeTenantResource{}
+}
+
+func (d *deploymentFreezeTenantResource) Metadata(_ context.Context, _ resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = util.GetTypeName("deployment_freeze_tenant")
+}
+
+func (d *deploymentFreezeTenantResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schemas.DeploymentFreezeTenantSchema{}.GetResourceSchema()
+}
+
+func (d *deploymentFreezeTenantResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	d.Config = ResourceConfiguration(req, resp)
+}
+
+func (d *deploymentFreezeTenantResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	internal.Mutex.Lock()
+	defer internal.Mutex.Unlock()
+
+	util.Create(ctx, tenantDescription)
+
+	var plan schemas.DeploymentFreezeTenantResourceModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	tflog.Debug(ctx, fmt.Sprintf("adding tenant (%s) to deployment freeze (%s)", plan.TenantID.ValueString(), plan.DeploymentFreezeID.ValueString()))
+	freeze, err := deploymentfreezes.GetById(d.Client, plan.DeploymentFreezeID.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError("cannot load deployment freeze", err.Error())
+		return
+	}
+
+	// Create new tenant scope
+	tenantScope := deploymentfreezes.TenantProjectEnvironment{
+		TenantId:      plan.TenantID.ValueString(),
+		ProjectId:     plan.ProjectID.ValueString(),
+		EnvironmentId: plan.EnvironmentID.ValueString(),
+	}
+
+	// Add to existing scopes
+	freeze.TenantProjectEnvironmentScope = append(freeze.TenantProjectEnvironmentScope, tenantScope)
+
+	freeze, err = deploymentfreezes.Update(d.Client, freeze)
+	if err != nil {
+		resp.Diagnostics.AddError("error while updating deployment freeze", err.Error())
+		return
+	}
+
+	plan.ID = types.StringValue(util.BuildCompositeId(plan.DeploymentFreezeID.ValueString(), plan.TenantID.ValueString(), plan.ProjectID.ValueString(), plan.EnvironmentID.ValueString()))
+	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
+	tflog.Debug(ctx, fmt.Sprintf("tenant scope (%s) added to deployment freeze (%s)", plan.TenantID.ValueString(), plan.DeploymentFreezeID.ValueString()))
+	util.Created(ctx, tenantDescription)
+}
+
+func (d *deploymentFreezeTenantResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	util.Reading(ctx, tenantDescription)
+
+	var data schemas.DeploymentFreezeTenantResourceModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	bits := util.SplitCompositeId(data.ID.ValueString())
+	freezeId := bits[0]
+	tenantId := bits[1]
+	projectId := bits[2]
+	environmentId := bits[3]
+
+	freeze, err := deploymentfreezes.GetById(d.Client, freezeId)
+	if err != nil {
+		apiError := err.(*core.APIError)
+		if apiError.StatusCode != http.StatusNotFound {
+			resp.Diagnostics.AddError("unable to load deployment freeze", err.Error())
+			return
+		}
+	}
+
+	// Verify the tenant scope still exists
+	exists := false
+	for _, scope := range freeze.TenantProjectEnvironmentScope {
+		if scope.TenantId == tenantId && scope.ProjectId == projectId && scope.EnvironmentId == environmentId {
+			exists = true
+			break
+		}
+	}
+
+	if !exists {
+		resp.State.RemoveResource(ctx)
+		return
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	util.Read(ctx, tenantDescription)
+}
+
+func (d *deploymentFreezeTenantResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	internal.Mutex.Lock()
+	defer internal.Mutex.Unlock()
+
+	util.Update(ctx, tenantDescription)
+
+	var plan, state schemas.DeploymentFreezeTenantResourceModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	freeze, err := deploymentfreezes.GetById(d.Client, state.DeploymentFreezeID.ValueString())
+	if err != nil {
+		apiError := err.(*core.APIError)
+		if apiError.StatusCode != http.StatusNotFound {
+			resp.Diagnostics.AddError("unable to load deployment freeze", err.Error())
+			return
+		}
+	}
+
+	// Remove old scope
+	bits := util.SplitCompositeId(state.ID.ValueString())
+	oldTenantId := bits[1]
+	oldProjectId := bits[2]
+	oldEnvironmentId := bits[3]
+
+	newScopes := make([]deploymentfreezes.TenantProjectEnvironment, 0)
+	for _, scope := range freeze.TenantProjectEnvironmentScope {
+		if scope.TenantId != oldTenantId || scope.ProjectId != oldProjectId || scope.EnvironmentId != oldEnvironmentId {
+			newScopes = append(newScopes, scope)
+		}
+	}
+
+	// Add new scope
+	newScopes = append(newScopes, deploymentfreezes.TenantProjectEnvironment{
+		TenantId:      plan.TenantID.ValueString(),
+		ProjectId:     plan.ProjectID.ValueString(),
+		EnvironmentId: plan.EnvironmentID.ValueString(),
+	})
+
+	freeze.TenantProjectEnvironmentScope = newScopes
+
+	freeze, err = deploymentfreezes.Update(d.Client, freeze)
+	if err != nil {
+		resp.Diagnostics.AddError("error while updating deployment freeze", err.Error())
+		return
+	}
+
+	plan.ID = types.StringValue(util.BuildCompositeId(plan.DeploymentFreezeID.ValueString(), plan.TenantID.ValueString(), plan.ProjectID.ValueString(), plan.EnvironmentID.ValueString()))
+	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
+	tflog.Debug(ctx, fmt.Sprintf("updated tenant scope (%s) in deployment freeze (%s)", plan.TenantID.ValueString(), plan.DeploymentFreezeID.ValueString()))
+	util.Updated(ctx, tenantDescription)
+}
+
+func (d *deploymentFreezeTenantResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	internal.Mutex.Lock()
+	defer internal.Mutex.Unlock()
+
+	util.Delete(ctx, tenantDescription)
+
+	var data schemas.DeploymentFreezeTenantResourceModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	freeze, err := deploymentfreezes.GetById(d.Client, data.DeploymentFreezeID.ValueString())
+	if err != nil {
+		apiError := err.(*core.APIError)
+		if apiError.StatusCode != http.StatusNotFound {
+			resp.Diagnostics.AddError("unable to load deployment freeze", err.Error())
+			return
+		}
+	}
+
+	// Remove the tenant scope
+	bits := util.SplitCompositeId(data.ID.ValueString())
+	tenantId := bits[1]
+	projectId := bits[2]
+	environmentId := bits[3]
+
+	newScopes := make([]deploymentfreezes.TenantProjectEnvironment, 0)
+	for _, scope := range freeze.TenantProjectEnvironmentScope {
+		if scope.TenantId != tenantId || scope.ProjectId != projectId || scope.EnvironmentId != environmentId {
+			newScopes = append(newScopes, scope)
+		}
+	}
+
+	freeze.TenantProjectEnvironmentScope = newScopes
+
+	freeze, err = deploymentfreezes.Update(d.Client, freeze)
+	if err != nil {
+		resp.Diagnostics.AddError(fmt.Sprintf("cannot remove tenant scope (%s) from deployment freeze (%s)", data.TenantID.ValueString(), data.DeploymentFreezeID.ValueString()), err.Error())
+	}
+
+	tflog.Debug(ctx, fmt.Sprintf("tenant scope (%s) removed from deployment freeze (%s)", data.TenantID.ValueString(), data.DeploymentFreezeID.ValueString()))
+	util.Deleted(ctx, tenantDescription)
+}
--- octopusdeploy_framework/resource_deployment_freeze_test.go
+++ octopusdeploy_framework/resource_deployment_freeze_test.go
@@ -18,12 +18,14 @@ func TestNewDeploymentFreezeResource(t *testing.T) {
 	start := fmt.Sprintf("%d-11-21T06:30:00+10:00", time.Now().Year()+1)
 	end := fmt.Sprintf("%d-11-21T08:30:00+10:00", time.Now().Year()+1)
 	updatedEnd := fmt.Sprintf("%d-11-21T08:30:00+10:00", time.Now().Year()+2)
+
 	projectName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
 	environmentName1 := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
 	environmentName2 := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
 	spaceName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
 	projectGroupName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
 	lifecycleName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	tenantName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
 
 	resource.Test(t, resource.TestCase{
 		CheckDestroy:             testDeploymentFreezeCheckDestroy,
@@ -36,7 +38,7 @@ func TestNewDeploymentFreezeResource(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "name", name),
 					resource.TestCheckResourceAttr(resourceName, "start", start),
 					resource.TestCheckResourceAttr(resourceName, "end", end)),
-				Config: testDeploymentFreezeBasic(localName, name, start, end, spaceName, []string{environmentName1}, projectName, projectGroupName, lifecycleName),
+				Config: testDeploymentFreezeBasic(localName, name, start, end, spaceName, []string{environmentName1}, projectName, projectGroupName, lifecycleName, tenantName, false),
 			},
 			{
 				Check: resource.ComposeTestCheckFunc(
@@ -44,18 +46,26 @@ func TestNewDeploymentFreezeResource(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "name", name+"1"),
 					resource.TestCheckResourceAttr(resourceName, "start", start),
 					resource.TestCheckResourceAttr(resourceName, "end", updatedEnd)),
-				Config: testDeploymentFreezeBasic(localName, name+"1", start, updatedEnd, spaceName, []string{environmentName1, environmentName2}, projectName, projectGroupName, lifecycleName),
+				Config: testDeploymentFreezeBasic(localName, name+"1", start, updatedEnd, spaceName, []string{environmentName1, environmentName2}, projectName, projectGroupName, lifecycleName, tenantName, false),
+			},
+			{
+				Check: resource.ComposeTestCheckFunc(
+					testDeploymentFreezeExists(resourceName),
+					testDeploymentFreezeTenantExists(fmt.Sprintf("octopusdeploy_deployment_freeze_tenant.tenant_%s", localName))),
+				Config: testDeploymentFreezeBasic(localName, name+"1", start, updatedEnd, spaceName, []string{environmentName1, environmentName2}, projectName, projectGroupName, lifecycleName, tenantName, true),
 			},
 		},
 	})
 }
 
-func testDeploymentFreezeBasic(localName string, freezeName string, start string, end string, spaceName string, environments []string, projectName string, projectGroupName string, lifecycleName string) string {
+func testDeploymentFreezeBasic(localName string, freezeName string, start string, end string, spaceName string, environments []string, projectName string, projectGroupName string, lifecycleName string, tenantName string, includeTenant bool) string {
 	spaceLocalName := fmt.Sprintf("space_%s", localName)
 	projectScopeLocalName := fmt.Sprintf("project_scope_%s", localName)
 	projectLocalName := fmt.Sprintf("project_%s", localName)
 	lifecycleLocalName := fmt.Sprintf("lifecycle_%s", localName)
 	projectGroupLocalName := fmt.Sprintf("project_group_%s", localName)
+	tenantLocalName := fmt.Sprintf("tenant_%s", localName)
+
 	environmentScopes := make([]string, 0, len(environments))
 	environmentResources := ""
 	for i, environmentName := range environments {
@@ -64,38 +74,73 @@ func testDeploymentFreezeBasic(localName string, freezeName string, start string
 		environmentScopes = append(environmentScopes, fmt.Sprintf("resource.octopusdeploy_environment.%s.id", environmentLocalName))
 	}
 
-	projectScopes := fmt.Sprintf(`resource "octopusdeploy_deployment_freeze_project" "%s" {
-		deploymentfreeze_id = octopusdeploy_deployment_freeze.%s.id
-		project_id = octopusdeploy_project.%s.id
-		environment_ids = [ %s ]
-	}`, projectScopeLocalName, localName, projectLocalName, strings.Join(environmentScopes, ","))
+	config := fmt.Sprintf(`
+       
+        # Space Configuration
+        %s
 
-	fmt.Println(projectScopes)
+        # Environment Configuration
+        %s
 
-	return fmt.Sprintf(`
-	%s
+        # Lifecycle Configuration
+        %s
 
-	%s
+        # Project Group Configuration
+        %s
 
-	%s
+        # Project Configuration
+        %s
 
-	%s
+        resource "octopusdeploy_deployment_freeze" "%s" {
+            name = "%s"
+            start = "%s"
+            end = "%s"
+        }
 
-	%s
-
-	resource "octopusdeploy_deployment_freeze" "%s" {
-		name = "%s"
-		start = "%s"
-		end = "%s"
-	}
-
-	%s`,
+        resource "octopusdeploy_deployment_freeze_project" "%s" {
+            deploymentfreeze_id = octopusdeploy_deployment_freeze.%s.id
+            project_id = octopusdeploy_project.%s.id
+            environment_ids = [ %s ]
+        }`,
 		createSpace(spaceLocalName, spaceName),
 		environmentResources,
 		createLifecycle(spaceLocalName, lifecycleLocalName, lifecycleName),
 		createProjectGroup(spaceLocalName, projectGroupLocalName, projectGroupName),
 		createProject(spaceLocalName, projectLocalName, projectName, lifecycleLocalName, projectGroupLocalName),
-		localName, freezeName, start, end, projectScopes)
+		localName, freezeName, start, end,
+		projectScopeLocalName, localName, projectLocalName,
+		strings.Join(environmentScopes, ","))
+
+	if includeTenant {
+		tenantConfig := fmt.Sprintf(`
+            resource "octopusdeploy_tenant" "%[1]s" {
+                name = "%[2]s"
+                space_id = octopusdeploy_space.%[3]s.id
+            }
+
+            resource "octopusdeploy_tenant_project" "%[1]s_project" {
+                tenant_id = octopusdeploy_tenant.%[1]s.id
+                project_id = octopusdeploy_project.%[4]s.id
+                environment_ids = [octopusdeploy_environment.environment_0_%[5]s.id]
+                space_id = octopusdeploy_space.%[3]s.id
+            }
+
+            resource "octopusdeploy_deployment_freeze_tenant" "%[1]s" {
+                deploymentfreeze_id = octopusdeploy_deployment_freeze.%[5]s.id
+                tenant_id = octopusdeploy_tenant.%[1]s.id
+                project_id = octopusdeploy_project.%[4]s.id
+                environment_id = octopusdeploy_environment.environment_0_%[5]s.id
+
+                depends_on = [
+                    octopusdeploy_tenant_project.%[1]s_project
+                ]
+            }`,
+			tenantLocalName, tenantName, spaceLocalName, projectLocalName, localName)
+
+		config = fmt.Sprintf("%s\n\n%s", config, tenantConfig)
+	}
+
+	return config
 }
 
 func testDeploymentFreezeExists(prefix string) resource.TestCheckFunc {
@@ -104,8 +149,39 @@ func testDeploymentFreezeExists(prefix string) resource.TestCheckFunc {
 		if _, err := deploymentfreezes.GetById(octoClient, freezeId); err != nil {
 			return err
 		}
-
 		return nil
+	}
+}
+
+func testDeploymentFreezeTenantExists(prefix string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[prefix]
+		if !ok {
+			return fmt.Errorf("Not found: %s", prefix)
+		}
+
+		bits := strings.Split(rs.Primary.ID, ":")
+		if len(bits) != 4 {
+			return fmt.Errorf("Invalid ID format for deployment freeze tenant: %s", rs.Primary.ID)
+		}
+
+		freezeId := bits[0]
+		tenantId := bits[1]
+		projectId := bits[2]
+		environmentId := bits[3]
+
+		freeze, err := deploymentfreezes.GetById(octoClient, freezeId)
+		if err != nil {
+			return err
+		}
+
+		for _, scope := range freeze.TenantProjectEnvironmentScope {
+			if scope.TenantId == tenantId && scope.ProjectId == projectId && scope.EnvironmentId == environmentId {
+				return nil
+			}
+		}
+
+		return fmt.Errorf("Tenant scope not found in deployment freeze")
 	}
 }
 
@@ -120,6 +196,5 @@ func testDeploymentFreezeCheckDestroy(s *terraform.State) error {
 			return fmt.Errorf("Deployment Freeze (%s) still exists", rs.Primary.ID)
 		}
 	}
-
 	return nil
 }
