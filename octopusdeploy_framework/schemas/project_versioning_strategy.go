573e456aff8f533cfd1bb02ab60d2b41cbecbf9d
denys.kostynchuk <denys.kostynchuk@octopus.com>
2025-03-11 15:35:58 +1300

    Make versioning strategy donor package attributes mutually exclusive.
    When donor_package.deployment_action is set it will override any value set to the donor_package_step_id

--- octopusdeploy_framework/schemas/project_versioning_strategy.go
+++ octopusdeploy_framework/schemas/project_versioning_strategy.go
@@ -3,10 +3,13 @@ package schemas
 import (
 	"github.com/OctopusDeploy/terraform-provider-octopusdeploy/octopusdeploy_framework/util"
 	"github.com/hashicorp/terraform-plugin-framework-validators/resourcevalidator"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	datasourceSchema "github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	resourceSchema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/objectplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
@@ -32,13 +35,17 @@ func (p ProjectVersioningStrategySchema) GetResourceSchema() resourceSchema.Sche
 			"donor_package_step_id": util.ResourceString().
 				Description("The associated donor package step ID.").
 				Optional().
+				Computed().
+				PlanModifiers(stringplanmodifier.RequiresReplace()).
 				Build(),
 			"template": util.ResourceString().
 				Optional().
 				Computed().
+				PlanModifiers(stringplanmodifier.UseStateForUnknown()).
 				Build(),
 			"donor_package": resourceSchema.SingleNestedAttribute{
 				Optional:    true,
+				Computed:    true,
 				Description: "Donor Packages.",
 				Attributes: map[string]resourceSchema.Attribute{
 					"deployment_action": util.ResourceString().
@@ -50,6 +57,9 @@ func (p ProjectVersioningStrategySchema) GetResourceSchema() resourceSchema.Sche
 						Required().
 						Build(),
 				},
+				PlanModifiers: []planmodifier.Object{
+					objectplanmodifier.UseStateForUnknown(),
+				},
 			},
 		},
 	}
@@ -57,8 +67,8 @@ func (p ProjectVersioningStrategySchema) GetResourceSchema() resourceSchema.Sche
 
 func (p ProjectVersioningStrategySchema) GetResourceConfigValidators() []resource.ConfigValidator {
 	return []resource.ConfigValidator{
-		resourcevalidator.RequiredTogether(
-			path.MatchRoot("donor_package"),
+		resourcevalidator.Conflicting(
+			path.MatchRelative().AtName("donor_package").AtName("deployment_action"),
 			path.MatchRoot("donor_package_step_id"),
 		),
 		resourcevalidator.Conflicting(
@@ -78,14 +88,21 @@ func (p ProjectVersioningStrategySchema) GetDatasourceSchema() datasourceSchema.
 }
 
 type ProjectVersioningStrategyModel struct {
-	ProjectID          types.String       `tfsdk:"project_id"`
-	SpaceID            types.String       `tfsdk:"space_id"`
-	DonorPackageStepID types.String       `tfsdk:"donor_package_step_id"`
-	Template           types.String       `tfsdk:"template"`
-	DonorPackage       *DonorPackageModel `tfsdk:"donor_package"`
+	ProjectID          types.String `tfsdk:"project_id"`
+	SpaceID            types.String `tfsdk:"space_id"`
+	DonorPackageStepID types.String `tfsdk:"donor_package_step_id"`
+	Template           types.String `tfsdk:"template"`
+	DonorPackage       types.Object `tfsdk:"donor_package"`
 }
 
 type DonorPackageModel struct {
 	DeploymentAction types.String `tfsdk:"deployment_action"`
 	PackageReference types.String `tfsdk:"package_reference"`
+}
+
+func ProjectVersioningStrategyDonorPackageAttributeTypes() map[string]attr.Type {
+	return map[string]attr.Type{
+		"deployment_action": types.StringType,
+		"package_reference": types.StringType,
+	}
 }
