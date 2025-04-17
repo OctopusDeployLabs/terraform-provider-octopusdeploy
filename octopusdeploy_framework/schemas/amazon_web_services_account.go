6b781d31ed3672a0f9ad5b58f58f4526a8706a20
wlthomson <will.tho@outlook.com>
2025-04-17 14:04:27 +1200

    Add AWS account framework schema

---
+++ octopusdeploy_framework/schemas/amazon_web_services_account.go
@@ -0,0 +1,65 @@
+package schemas
+
+import (
+	"github.com/OctopusDeploy/terraform-provider-octopusdeploy/octopusdeploy_framework/util"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	resourceSchema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+const AmazonWebServicesAccountResourceDescription = "amazon web services account"
+
+type AmazonWebServicesAccountSchema struct{}
+
+type AmazonWebServiceAccountModel struct {
+	AccessKey                       types.String `tfsdk:"access_key"`
+	Description                     types.String `tfsdk:"description"`
+	Environments                    types.List   `tfsdk:"environments"`
+	Name                            types.String `tfsdk:"name"`
+	SecretKey                       types.String `tfsdk:"secret_key"`
+	SpaceId                         types.String `tfsdk:"space_id"`
+	TenantedDeploymentParticipation types.String `tfsdk:"tenanted_deployment_participation"`
+	Tenants                         types.List   `tfsdk:"tenants"`
+	TenantTags                      types.List   `tfsdk:"tenant_tags"`
+}
+
+func (a AmazonWebServicesAccountSchema) GetAmazonWebServicesAccountResourceSchema() resourceSchema.Schema {
+	return resourceSchema.Schema{
+		Description: util.GetResourceSchemaDescription(AmazonWebServicesAccountResourceDescription),
+		Attributes: map[string]resourceSchema.Attribute{
+			"access_key": resourceSchema.StringAttribute{
+				Description: "The access key associated with this AWS account.",
+				Required:    true,
+			},
+			"description": GetDescriptionResourceSchema(AmazonWebServicesAccountResourceDescription),
+			"environments": resourceSchema.ListAttribute{
+				Description: "A list of environment IDs associated with this AWS account.",
+				Optional:    true,
+			},
+			"name": GetNameResourceSchema(true),
+			"secret_key": resourceSchema.StringAttribute{
+				Description: "The secret key associated with this AWS account.",
+				Required:    true,
+			},
+			"space_id": GetSpaceIdResourceSchema(AmazonWebServicesAccountResourceDescription),
+			"tenanted_deployment_participation": resourceSchema.StringAttribute{
+				Description: "The tenanted deployment mode of the resource. Valid account types are `Untenanted`, `TenantedOrUntenanted`, or `Tenanted`.",
+				Optional:    true,
+				Computed:    true,
+				Validators: []validator.String{
+					stringvalidator.OneOf("Untenanted", "TenantedOrUntenanted", "Tenanted"),
+				},
+			},
+			"tenants": resourceSchema.ListAttribute{
+				Description: "A list of tenant IDs associated with this AWS account.",
+				Optional:    true,
+				Computed:    true,
+			},
+			"tenant_tags": resourceSchema.ListAttribute{
+				Description: "A list of tenant tags associated with this AWS account.",
+				Optional:    true,
+			},
+		},
+	}
+}
