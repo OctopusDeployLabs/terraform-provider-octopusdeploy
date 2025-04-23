5a18f75c7f1daec960bad9dd2e19c50989e60f75
Will Thomson <43223637+wlthomson@users.noreply.github.com>
2025-04-23 12:12:32 +1200

    Extend ACR feed resource to support OIDC properties (#891)

--- octopusdeploy_framework/schemas/azure_container_registry_feed.go
+++ octopusdeploy_framework/schemas/azure_container_registry_feed.go
@@ -3,6 +3,7 @@ package schemas
 import (
 	datasourceSchema "github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	resourceSchema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
@@ -26,6 +27,28 @@ func (d AzureContainerRegistryFeedSchema) GetResourceSchema() resourceSchema.Sch
 			"registry_path": resourceSchema.StringAttribute{
 				Optional: true,
 			},
+			"oidc_authentication": resourceSchema.SingleNestedAttribute{
+				Optional: true,
+				Attributes: map[string]resourceSchema.Attribute{
+					"client_id": resourceSchema.StringAttribute{
+						Description: "Unique identifier representing the application requesting authentication",
+						Optional:    true,
+						Computed:    true,
+					},
+					"tenant_id": resourceSchema.StringAttribute{
+						Description: "Unique identifier representing the Azure AD instance hosting the authenticating application",
+						Optional:    true,
+						Computed:    true,
+					},
+					"audience": resourceSchema.StringAttribute{
+						Description: "Audience representing the intended recipient of the OIDC token",
+						Optional:    true,
+						Computed:    true,
+						Default:     stringdefault.StaticString(""),
+					},
+					"subject_keys": GetOidcSubjectKeysSchema("Keys to include in a deployment or runbook. Valid options are `space`, `feed`.", false),
+				},
+			},
 		},
 	}
 }
@@ -35,13 +58,21 @@ func (d AzureContainerRegistryFeedSchema) GetDatasourceSchema() datasourceSchema
 }
 
 type AzureContainerRegistryFeedTypeResourceModel struct {
-	APIVersion   types.String `tfsdk:"api_version"`
-	FeedUri      types.String `tfsdk:"feed_uri"`
-	Name         types.String `tfsdk:"name"`
-	Password     types.String `tfsdk:"password"`
-	SpaceID      types.String `tfsdk:"space_id"`
-	Username     types.String `tfsdk:"username"`
-	RegistryPath types.String `tfsdk:"registry_path"`
+	APIVersion         types.String                                           `tfsdk:"api_version"`
+	FeedUri            types.String                                           `tfsdk:"feed_uri"`
+	Name               types.String                                           `tfsdk:"name"`
+	Password           types.String                                           `tfsdk:"password"`
+	SpaceID            types.String                                           `tfsdk:"space_id"`
+	Username           types.String                                           `tfsdk:"username"`
+	RegistryPath       types.String                                           `tfsdk:"registry_path"`
+	OidcAuthentication *AzureContainerRegistryOidcAuthenticationResourceModel `tfsdk:"oidc_authentication"`
 
 	ResourceModel
+}
+
+type AzureContainerRegistryOidcAuthenticationResourceModel struct {
+	ClientId   types.String `tfsdk:"client_id"`
+	TenantId   types.String `tfsdk:"tenant_id"`
+	Audience   types.String `tfsdk:"audience"`
+	SubjectKey types.List   `tfsdk:"subject_keys"`
 }
