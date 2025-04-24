47c284ffa707cc334cc3db4ec0eb3745ea1c25bb
wlthomson <will.tho@outlook.com>
2025-04-24 16:19:05 +1200

    Add create operation

--- octopusdeploy_framework/resource_azure_subscription_account.go
+++ octopusdeploy_framework/resource_azure_subscription_account.go
@@ -3,10 +3,14 @@ package octopusdeploy_framework
 import (
 	"context"
 	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/accounts"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/core"
 	"github.com/OctopusDeploy/terraform-provider-octopusdeploy/octopusdeploy_framework/schemas"
 	"github.com/OctopusDeploy/terraform-provider-octopusdeploy/octopusdeploy_framework/util"
+	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
 type azureSubscriptionAccountResource struct {
@@ -32,7 +36,25 @@ func (r *azureSubscriptionAccountResource) Configure(_ context.Context, req reso
 }
 
 func (r *azureSubscriptionAccountResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	return
+	var plan schemas.AzureSubscriptionAccountModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	tflog.Debug(ctx, "Creating Azure subscription account", map[string]interface{}{
+		"name": plan.Name.ValueString(),
+	})
+
+	account := expandAzureSubscriptionAccount(ctx, plan)
+	createdAccount, err := accounts.Add(r.Client, account)
+	if err != nil {
+		resp.Diagnostics.AddError("Error creating Azure subscription account", err.Error())
+		return
+	}
+
+	state := flattenAzureSubscriptionAccount(ctx, createdAccount.(*accounts.AzureSubscriptionAccount), plan)
+	resp.Diagnostics.Append(resp.State.Set(ctx, state)...)
 }
 
 func (r *azureSubscriptionAccountResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
@@ -52,9 +74,45 @@ func (*azureSubscriptionAccountResource) ImportState(ctx context.Context, req re
 }
 
 func expandAzureSubscriptionAccount(ctx context.Context, model schemas.AzureSubscriptionAccountModel) *accounts.AzureSubscriptionAccount {
-	return nil
+	var name = model.Name.ValueString()
+	var subscriptionId, _ = uuid.Parse(model.SubscriptionID.ValueString())
+
+	account, _ := accounts.NewAzureSubscriptionAccount(name, subscriptionId)
+
+	account.SetID(model.ID.ValueString())
+	account.AzureEnvironment = model.AzureEnvironment.ValueString()
+	account.CertificateBytes = core.NewSensitiveValue(model.Certificate.ValueString())
+	account.CertificateThumbprint = model.CertificateThumbprint.ValueString()
+	account.SetDescription(model.Description.ValueString())
+	account.SetEnvironmentIDs(expandStringList(model.Environments))
+	account.ManagementEndpoint = model.ManagementEndpoint.ValueString()
+	account.SetName(model.Name.ValueString())
+	account.SetSpaceID(model.SpaceID.ValueString())
+	account.StorageEndpointSuffix = model.StorageEndpointSuffix.ValueString()
+	account.SetTenantedDeploymentMode(core.TenantedDeploymentMode(model.TenantedDeploymentParticipation.ValueString()))
+	account.SetTenantTags(expandStringList(model.TenantTags))
+	account.SetTenantIDs(expandStringList(model.Tenants))
+
+	return account
+
 }
 
-func flattenAmazonWebServicesAccount(ctx context.Context, account *accounts.AmazonWebServicesAccount, model schemas.AzureSubscriptionAccountModel) schemas.AzureSubscriptionAccountModel {
+func flattenAzureSubscriptionAccount(ctx context.Context, account *accounts.AzureSubscriptionAccount, model schemas.AzureSubscriptionAccountModel) schemas.AzureSubscriptionAccountModel {
+	model.ID = types.StringValue(account.GetID())
+	model.AzureEnvironment = types.StringValue(account.AzureEnvironment)
+	model.CertificateThumbprint = types.StringValue(account.CertificateThumbprint)
+	model.Description = types.StringValue(account.GetDescription())
+	model.Environments = flattenStringList(account.GetEnvironmentIDs(), model.Environments)
+	model.ManagementEndpoint = types.StringValue(account.ManagementEndpoint)
+	model.Name = types.StringValue(account.GetName())
+	model.SubscriptionID = types.StringValue(account.SubscriptionID.String())
+	model.SpaceID = types.StringValue(account.GetSpaceID())
+	model.StorageEndpointSuffix = types.StringValue(account.StorageEndpointSuffix)
+	model.TenantedDeploymentParticipation = types.StringValue(string(account.GetTenantedDeploymentMode()))
+	model.Tenants = flattenStringList(account.GetTenantIDs(), model.Tenants)
+	model.TenantTags = flattenStringList(account.TenantTags, model.TenantTags)
+
+	// Note: We don't flatten the certificate as it's sensitive and not returned by the API
+
 	return model
 }
