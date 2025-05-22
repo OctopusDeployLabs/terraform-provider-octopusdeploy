639ca384627192a99610cc4715a5ad13fed6115a
denys.kostynchuk <denys.kostynchuk@octopus.com>
2025-05-23 09:20:32 +1200

    Rename test functions for resource compatibility

--- octopusdeploy_framework/config_test.go
+++ octopusdeploy_framework/config_test.go
@@ -5,31 +5,31 @@ import (
 	"testing"
 )
 
-func TestAssertResourceCompatibilityWithServerNewerOrSameShouldPass(t *testing.T) {
+func TestEnsureResourceCompatibilityByVersionNewerOrSameShouldPass(t *testing.T) {
 	testServerVersionShouldPass(t, "2025.1", "2025.1")
 	testServerVersionShouldPass(t, "2025.1", "2025.1.24564")
 	testServerVersionShouldPass(t, "2025.1", "2025.2")
 	testServerVersionShouldPass(t, "2025.1.0", "2025.1")
 }
 
-func TestAssertResourceCompatibilityWithServerOlderShouldFail(t *testing.T) {
+func TestEnsureResourceCompatibilityByVersionOlderShouldFail(t *testing.T) {
 	testServerVersionShouldFail(t, "2025.1", "2024.4")
 	testServerVersionShouldFail(t, "2025.1.12345", "2025.1.12344")
 	testServerVersionShouldFail(t, "2025.1.7108", "2025.1")
 }
 
-func TestAssertResourceCompatibilityWithServerLocalAlwaysPasses(t *testing.T) {
+func TestEnsureResourceCompatibilityByVersionLocalAlwaysPasses(t *testing.T) {
 	testServerVersionShouldPass(t, "2025.1", "0.0.0-local")
 	testServerVersionShouldPass(t, "2020.3.023498", "0.0.0-local")
 }
 
-func TestAssertResourceCompatibilityWithServerTreatsInvalidVersionAsEmpty(t *testing.T) {
+func TestEnsureResourceCompatibilityByVersionTreatsInvalidVersionAsEmpty(t *testing.T) {
 	testServerVersionShouldPass(t, "2025.not-a-number", "2024.4.20456")
 	testServerVersionShouldFail(t, "2025.2", "local")
 	testServerVersionShouldFail(t, "2025.0.1501", "2025.local.1500")
 }
 
-func TestAssertResourceCompatibilityWithServerIgnoresBranches(t *testing.T) {
+func TestEnsureResourceCompatibilityByVersionIgnoresBranches(t *testing.T) {
 	testServerVersionShouldPass(t, "2025.1", "2025.1-my-branch")
 	testServerVersionShouldFail(t, "2025.2.100-testing", "2025.2.99-experiment")
 }
