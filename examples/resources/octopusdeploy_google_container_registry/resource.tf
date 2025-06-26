31e6ddb03d1d23ab63c2b56a5d3fd26e16c06ce0
Blake Irwin <blake.irwin@vista.co>
2025-06-26 12:15:08 +1200

    Update "octopusdeploy_google_container_registry" > "oidc_authentication" attribute examples

--- examples/resources/octopusdeploy_google_container_registry/resource.tf
+++ examples/resources/octopusdeploy_google_container_registry/resource.tf
@@ -9,7 +9,7 @@ resource "octopusdeploy_google_container_registry" "example_with_oidc" {
   name          = "Test Google Container Registry (OK to Delete)"
   feed_uri      = "https://google.docker.test"
   registry_path = "testing/test-image"
-  oidc_authentication {
+  oidc_authentication = {
     audience      = "audience"
     subject_keys = ["feed", "space"]
   }
